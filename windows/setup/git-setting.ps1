0e884875fff9d3eb66686dd9b64e64db62b355fc
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-12-02 17:07:14 +0900

    feat: add git setting setup system

---
+++ windows/setup/git-setting.ps1
@@ -0,0 +1,10 @@
+# Git setting: info JP(https://qiita.com/hayamofu/items/d8103e789196bcd8b489)
+git config --global core.ignorecase false
+git config --global core.quotepath false
+git config --global core.safecrlf true
+git config --global core.autocrlf false
+
+Write-Host "Please include the following by yourself." -ForegroundColor Blue
+Write-Host "git config --global user.name <name>"
+Write-Host "git config --global user.email <email>"
+Write-Host "git config --global -e "
