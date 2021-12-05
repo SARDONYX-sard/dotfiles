259b3fb3940d882516cc539501d6112c31a2fb84
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-12-06 07:29:08 +0900

    chore: move fzf setting

--- windows/config/profile.ps1
+++ windows/config/profile.ps1
@@ -15,6 +15,17 @@ Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }; # Ta
 # Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t'  # Search for file paths in the current directory
 # Set-PsFzfOption -PSReadlineChordReverseHistory 'Ctrl+r' #  Search command history
 
+# fzf
+if ( Get-Command bat -ea 0 ) {
+  $env:FZF_DEFAULT_OPTS = "--tabstop=4 --preview `"bat --pager=never --color=always --style=numbers --line-range :300 {}`""
+  if ( Get-Command rg -ea 0 ) {
+    $env:FZF_CTRL_T_COMMAND = 'rg --files --hidden --follow --glob "!.git/*"'
+  }
+}
+else {
+  $env:FZF_DEFAULT_OPTS = "--tabstop=4 --preview `"cat {}`""
+}
+
 # --------------------------------------------------------------------------------------------------
 # Aliases
 # --------------------------------------------------------------------------------------------------
@@ -341,17 +352,4 @@ $env_to_del = @(
 
 foreach ($var in $env_to_del) {
   Remove-Item env:$var -ErrorAction SilentlyContinue
-}
-
-
-# -- fzf の設定
-if ( Get-Command bat -ea 0 ) {
-  $env:FZF_DEFAULT_OPTS = "--tabstop=4 --preview `"bat --pager=never --color=always --style=numbers --line-range :300 {}`""
-  # $env:FZF_CTRL_T_OPTS = '--preview "bat  --color=always --style=header,grid --line-range :100 {}"'
-  if ( Get-Command rg -ea 0 ) {
-    $env:FZF_CTRL_T_COMMAND = 'rg --files --hidden --follow --glob "!.git/*"'
-  }
-}
-else {
-  $env:FZF_DEFAULT_OPTS = "--tabstop=4 --preview `"cat {}`""
 }
