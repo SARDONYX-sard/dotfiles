14e627e8ace2c8ad13cb7d2e093692dfd36edbb5
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-12-11 05:56:48 +0900

    chore: msys2 apps installer

---
+++ windows/setup/options/libs/msys2.ps1
@@ -0,0 +1,9 @@
+# need 1.07 GB of disk space
+
+pacman -Sy mingw-w64-x86_64-toolchain --noconfirm
+pacman -Sy make --noconfirm
+pacman -Sy ccache --noconfirm
+pacman -Sy subversion --noconfirm
+pacman -Sy findutils --noconfirm
+pacman -Sy ncurses-devel --noconfirm
+pacman -Sy openssh --noconfirm
