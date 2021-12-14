a48976c5f9f3f5b747d7e090a7270a5b756bbdd8
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-12-15 04:10:43 +0900

    chore: add instead of command libs

--- windows/setup/scoop-install.ps1
+++ windows/setup/scoop-install.ps1
@@ -95,8 +95,10 @@ scoop install resource-hacker # exe icon edit
 scoop install fzf # A command-line fuzzy finder(https://github.com/junegunn/fzf)
 
 # Instead of command
-scoop install bat # instead of `cat` Syntax highlighting and Git integration(https://github.com/sharkdp/bat)
-scoop install lsd # instead of `ls` commnad with color
+scoop install bat   # instead of `cat` Syntax highlighting and Git integration(https://github.com/sharkdp/bat)
+scoop install fd    # instead of `find` commnad with color
+scoop install lsd   # instead of `ls` commnad with color
+scoop install procs # instead of `ps` commnad with color
 
 scoop install concfg # Import and export Windows console settings (https://github.com/lukesampson/concfg)
 # scoop install pshazz # Git and Mercurial tab completion, etc... (https://github.com/lukesampson/pshazz)
