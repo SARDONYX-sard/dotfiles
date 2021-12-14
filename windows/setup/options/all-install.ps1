082bb73c7c64cf7f4d8273a12d0750c517afb0d3
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-12-15 03:57:24 +0900

    chore: add `dotnet-core-uninstall` option

--- windows/setup/options/all-install.ps1
+++ windows/setup/options/all-install.ps1
@@ -2,3 +2,10 @@ windows\setup\options\libs\msys2.ps1
 windows\setup\options\libs\nodejs.ps1 -Manager pnpm
 windows\setup\options\libs\python.ps1 -Manager pip
 windows\setup\options\libs\ruby.ps1
+
+if (Test-Path dotnet-core-uninstall) {
+  Write-Host "Manual install dotnet-core-uninstall(to remove Microsoft SDK)"
+  Write-Host "https://github.com/dotnet/cli-lab/releases"
+  Write-Host ""
+  Write-Host "How to use: https://qiita.com/okazuki/items/b1a04dc58064c37372e0"
+}
