e4f0ed17c1009608508e890c5a1ff0d74c026b24
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-12-18 07:05:07 +0900

    feat(winodws): implement `debug` mode for symlink

--- windows/setup/symlink.ps1
+++ windows/setup/symlink.ps1
@@ -1,20 +1,66 @@
-Param([switch]$f, [switch]$force)
+Param([switch]$f, [switch]$force, [switch]$d, [switch]$isDebug)
+$force = $f.IsPresent -or $force.IsPresent
+$isDebug = $d.IsPresent -or $isDebug.IsPresent
 
+# Import SymLink function
 $HelperDir = "$HOME/dotfiles/windows/config/powershell-profile/helper";
 . "$($HelperDir)/functions.ps1"
 
-Write-Host "Linking Symbolic links..." -ForegroundColor Green
 
-$force = $f.IsPresent -or $force.IsPresent
-
-if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
-      [Security.Principal.WindowsBuiltInRole] "Administrator")) {
-  Invoke-Expression "sudo $PsCommandPath $(if ($force) {"-force"})"
-  exit $?
+# Are you root?
+if ($isDebug -eq $false) {
+  if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+        [Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Invoke-Expression "sudo $PsCommandPath $(if ($force) {"-force"})"
+    exit $?
+  }
 }
 
-$terminalPath = Join-Path $env:LocalAppData "Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"
-$terminalPreviewPath = $terminalPath.Replace("Terminal", "TerminalPreview")
+
+# --------------------------------------------------------------------------------------------------
+# Files
+# --------------------------------------------------------------------------------------------------
+<#
+.NOTES
+Specification
+
+target(require):
+  target is the actual directory or file. Relative or absolute path from `$HOME/dotfiles`
+  (e.g.: "windows/config/.bashrc")
+path(option):
+  the directory where the symbolic link will be output
+  (e.g.: "$HOME")
+name(option):
+  the name of the symbolic file
+  (e.g.: ".bashrc")
+
+1. If there is a path, combine it with the $HOME directory.
+Example:
+
+      @{ path = scoop/current }
+      ↓
+      @{ path = $HOME/scoop/current }
+
+
+2. If there is fullpath If there is a fullpath, overwrite the string excluding the last path of the fullpath with overwrite path.
+Example:
+
+      @{ fullpath = windows/modules/dirs }
+      ↓
+      @{ path = windows/modules }
+
+3. if there is no path or fullpath, path will contain $HOME
+Example:
+
+      @{ "windows/config/.bashrc" }
+      ↓
+      @{ target= "windows/config/.bashrc"; path = "$HOME/windows/config/.bashrc"; name= "bashrc" }
+
+4. If you write it, only one of fullpath or path is needed.
+#>
+
+$pwshProfilePath = @($(pwsh -NoProfile -Command "`$profile"))[0]
+$powerShellProfilePath = @($(powerShell -NoProfile -Command "`$profile"))[0]
 
 $files = @(
   # vim
@@ -23,84 +69,101 @@ $files = @(
   @{ target = "ginit.vim"; path = "AppData/Local/nvim" }
 
   # Terminal
-  @{ target = "windows\data\windows-terminal\settings.json"; fullpath = $terminalPath; } # mode = "copy" }
-  @{ target = "windows\data\windows-terminal-preview\settings.json"; fullpath = $terminalPreviewPath; } # mode = "copy" }
+  $terminalPath = Join-Path $env:LocalAppData "Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"
+  $terminalPreviewPath = $terminalPath.Replace("Terminal", "TerminalPreview")
+  @{ target = "windows\data\windows-terminal.json"; fullpath = $terminalPath; name = "settings.json" } # mode = "copy" }
+  @{ target = "windows\data\windows-terminal-preview.json"; fullpath = $terminalPreviewPath; name = "settings.json" } # mode = "copy" }
 
+  # AutoHotkey (Keyboard keyconfig)
   @{ target = "windows/config/init.ahk"; fullpath = Join-Path $env:AppData  "Microsoft/Windows/Start Menu/Programs/Startup/init.ahk" }
 
   # oh-my-posh theme (custom)
-  @{ target  = "windows\data\custom-oh-my-posh\themes\gmay.omp.json";
+  @{ target  = "windows\data\oh-my-posh-themes\gmay.omp.json";
     fullpath = Join-Path (Split-Path (pwsh -NoProfile -Command "`$profile")) "\Modules\oh-my-posh\themes\gmay.omp.json"
   }
-  @{ target  = "windows\data\custom-oh-my-posh\themes\night-owl.omp.json";
+  @{ target  = "windows\data\oh-my-posh-themes\night-owl.omp.json";
     fullpath = Join-Path (Split-Path (pwsh -NoProfile -Command "`$profile")) "\Modules\oh-my-posh\themes\night-owl.omp.json"
   }
+
+  # msys2 HomeDir
+  $UserName = (Split-Path $HOME -Leaf)
+  @{ target = $HOME; fullpath = Join-Path $HOME "scoop\apps\msys2\current\home\$UserName"; }
 
   "windows/config/.bash_profile"
   "windows/config/.bashrc"
 )
 
+
 # If you have a profile, add it and add it to $files.
 if ( (Get-Command pwsh -ea 0) -and (pwsh -NoProfile -Command "`$profile")) {
-  $files += @(@{target = "windows/config/powershell-profile/profile.ps1"; fullpath = @($(pwsh -NoProfile -Command "`$profile"))[0] })
+  $files += @(@{target = "windows/config/powershell-profile/profile.ps1"; fullpath = $pwshProfilePath })
+
+  # create symlink from `pwsh's modules` to `powershell's modules`
+  $pwshModulePath = $pwshProfilePath | Split-Path | Join-Path { $_ } "Modules" | Split-Path
+  $powershellModulePath = $powerShellProfilePath | Split-Path | Join-Path { $_ } "Modules" | Split-Path
+  $files += @(@{ target = $pwshModulePath; fullpath = $powershellModulePath; })
 }
 
 if ( (Get-Command powershell -ea 0) -and (powershell -NoProfile -Command "`$profile")) {
-  $files += @(@{target = "windows/config/powershell-profile/profile.ps1"; fullpath = @($(powershell -NoProfile -Command "`$profile"))[0] })
+  $files += @(@{target = "windows/config/powershell-profile/profile.ps1"; fullpath = $powerShellProfilePath })
 }
 
 
-# ------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------
 # Create Symbolic link
-# ------------------------------------------------------------------------------------------------
-foreach ($file in $files) {
-  if ($file.GetType() -eq [string]) {
-    if (!$file) { continue }
-
-    $file = @{target = $file; path = "" }
-  }
-
-  $isDrive = (Split-Path $file.target) -match "^C:\\"
-  if ($isDrive -ne $true) {
-    $target_name = Split-Path $file.target -Leaf
-    $file.target = Join-Path "$HOME/dotfiles" $file.target
-  }
-
-  if ( !$file.path ) { $file.path = "$HOME" }
-  else { $file.path = Join-Path "$HOME" $file.path }
-
-  if ($file.fullpath) {
-    $file.path = Split-Path $file.fullpath
-    $name = Split-Path $file.fullpath -Leaf
-    if ($name) {
-      $file.name = $name
+# --------------------------------------------------------------------------------------------------
+function create_symlink($files) {
+  if ($isDebug) {
+    Write-Host "Debug mode is running..." -ForegroundColor Blue
+    if (Test-Path result.txt) {
+      Write-Host "result.txt already exists. Delete it." -ForegroundColor Yellow
+      Remove-Item result.txt -Force
     }
-    $file.Remove("fullpath")
+  }
+  else {
+    Write-Host "Symbolic links mode is running..." -ForegroundColor Green
   }
 
-  if (!$file.ContainsKey("name")) { $file.name = $target_name }
+  foreach ($file in $files) {
+    if ($file.GetType() -eq [string]) {
+      if (!$file) { continue }
 
-  if (!(Test-Path $file.path)) {
-    mkdir $file.path
+      $file = @{target = $file; path = "" }
+    }
+
+    $isDrive = (Split-Path $file.target) -match "^[A-Z]:\\"
+    if ($isDrive -eq $false) {
+      $file.target = Join-Path "$HOME/dotfiles" $file.target
+      if (!$file.name) { $file.name = Split-Path $file.target -Leaf }
+    }
+
+
+    if ( !$file.path ) { $file.path = "$HOME" }
+    else { $file.path = Join-Path "$HOME" $file.path }
+
+    if (!$file.name) { $file.name = Split-Path $file.path -Leaf }
+
+    # Overwrite `path`& `name` property if `fullpath` is specified.
+    if ($file.fullpath) {
+      $file.name = Split-Path $file.fullpath -Leaf
+      $file.path = Split-Path $file.fullpath
+      $file.Remove("fullpath")
+    }
+
+
+    if ($isDebug) {
+      Write-Output @file "`n" | Out-File result.txt -Append # for debug
+      Write-Host @file "`n"
+      continue
+    }
+
+    # Make symlink
+    if (!(Test-Path $file.path)) {
+      mkdir $file.path
+    }
+    if ($Force) { Set-SymLink -Hash $file -Force }
+    else { Set-SymLink -Hash $file }
   }
-
-  if ($Force) { Set-SymLink -Hash $file -Force }
-  else { Set-SymLink -Hash $file }
-  # Write-Output @file | Out-File result.txt -Append # for debug
 }
 
-# create symlink from `pwsh's modules` to `powershell's modules`
-$pwshModulePath = @($(pwsh -NoProfile -Command "`$profile"))[0] | Split-Path | Join-Path { $_ } "Modules" | Split-Path
-$powershellPath = @($(powershell -NoProfile -Command "`$profile"))[0] | Split-Path
-
-# msys2 HomeDir
-$UserName = (Split-Path $HOME -Leaf)
-
-if ($Force) {
-  Set-SymLink -Hash @{ target = $HOME; path = Join-Path $HOME "scoop\apps\msys2\current\home\$UserName"; } -Force
-  Set-SymLink -Hash @{ target = $pwshModulePath; path = $powershellPath; name = "Modules" } -Force
-}
-else {
-  Set-SymLink -Hash @{ target = $pwshModulePath; path = $powershellPath; name = "Modules" }
-  Set-SymLink -Hash @{ target = $HOME; path = Join-Path $HOME "scoop\apps\msys2\current\home\$UserName"; }
-}
+create_symlink $files
