9d57d78255773c8842403875eebcc2f05e76268b
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-12-18 04:28:13 +0900

    chore: modify `pwsh-modules`

--- windows/install-app.ps1
+++ windows/install-app.ps1
@@ -3,16 +3,38 @@ if (!(Test-Path $HOME/dotfiles)) {
   git clone https://github.com/SARDONYX-sard/dotfiles $HOME/dotfiles
 }
 
+# --------------------------------------------------------------------------------------------------
+# Require:
+# --------------------------------------------------------------------------------------------------
+# Install software with scoop
 Invoke-Expression "$HOME/dotfiles/windows/setup/scoop-install.ps1"
+
+# Git settings
 Invoke-Expression "$HOME/dotfiles/windows/setup/git-setting.ps1"
 
-#? $HOME env not working. So use `~`
-~/dotfiles/windows/setup/symlink.ps1
+# Connect each file (or directory) to dotfiles with a symbolic link.
+~/dotfiles/windows/setup/symlink.ps1 #? $HOME env not working. So use `~`
 
 winget import "$HOME/dotfiles/windows/data/winget-app-list.json"
 
 # Install powershell modules
 Invoke-Expression "$HOME/dotfiles/windows/setup/pwsh-modules.ps1"
+
+# --------------------------------------------------------------------------------------------------
+# Option: Install languages or Libraries
+# --------------------------------------------------------------------------------------------------
+Write-Host "Option: You can include the global package of programming languages or Libraries." -ForegroundColor Blue
+Write-Host @'
+How to install with Scoop:
+  Execute the following commands.↓
+
+  Example:
+    Node.js only
+      windows\setup\options\global-libs\nodejs.ps1 -Manager pnpm
+
+    All languages(Node.js, Ruby, python and msys2 libs)
+      windows\setup\options\global-libs\all-languages.ps1
+'@
 
 # -- install dein.vim
 git clone https://github.com/Shougo/dein.vim $HOME/.cache/dein/repos/github.com/Shougo/dein.vim
--- windows/setup/pwsh-modules.ps1
+++ windows/setup/pwsh-modules.ps1
@@ -1,3 +1,11 @@
-Import-Module -Name oh-my-posh -Force
-Install-Module -Name WslInterop -Force
-Install-Module -Name PSFzf -Force
+# --------------------------------------------------------------------------------------------------
+# Modules
+# --------------------------------------------------------------------------------------------------
+Install-Module -Name PSFzf -Force # PsFzf(to use fzf on windows)
+
+# Modules that run only on PowerShell Core
+if ($PSVersionTable.PSEdition -eq "Core") {
+  Import-Module -Name oh-my-posh -Force
+  Install-Module -Name WslInterop -Force
+  Install-Module -Name WslInterop -Force
+}
