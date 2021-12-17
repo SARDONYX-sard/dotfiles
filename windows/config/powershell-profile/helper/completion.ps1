f20614a241300537d3fc11788a8009ea31587929
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-12-18 01:17:16 +0900

    chore: add completion settings
    It doesn't make much sense...

--- windows/config/powershell-profile/helper/completion.ps1
+++ windows/config/powershell-profile/helper/completion.ps1
@@ -3,6 +3,12 @@ using namespace System.Management.Automation.Language
 
 
 if ($PSVersionTable.PSEdition -eq "Core") {
+  # Shows navigable menu of all options when hitting Tab
+  Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+  # Autocompletion for arrow keys
+  Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+  Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
   # --------------------------------------------------------------------------------------------------
   # rustup completion
