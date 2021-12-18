b987575ebf266ac9adcf4e2c487c084708625702
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-12-19 03:52:58 +0900

    feat(bin): create all installers

---
+++ linux/bin/all-installer.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+bash "$HOME"/dotfiles/linux/bin/installers/apt.sh
+bash "$HOME"/dotfiles/linux/bin/installers/asdf.sh
+bash "$HOME"/dotfiles/linux/bin/installers/rustup.sh
+bash "$HOME"/dotfiles/linux/bin/installers/zinit.sh
