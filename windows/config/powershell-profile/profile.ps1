ec20879a64997e84e3f009f5695c7e3b51f7ffad
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-12-19 03:33:58 +0900

    fix: fix msys2 path error

--- windows/config/powershell-profile/profile.ps1
+++ windows/config/powershell-profile/profile.ps1
@@ -15,9 +15,7 @@ if ((Get-PSReadlineOption).BellStyle -eq "Audible") {
 # --------------------------------------------------------------------------------------------------
 # Module settings
 # --------------------------------------------------------------------------------------------------
-# msys2
-#! Without this, msys2 will not inherit windows environment variables.
-if (Get-Command msys2 -ea 0) { $env:MSYS2_PATH_TYPE = "inherit" }
+
 
 if ($PSVersionTable.PSEdition -eq "Core") {
   # Install-Module WslInterop
@@ -58,3 +56,8 @@ $env_to_del = @(
 foreach ($var in $env_to_del) {
   Remove-Item env:$var -ErrorAction SilentlyContinue
 }
+
+# msys2
+#! This statement must be at the end of the profile or the command will be lost for some reason.
+#! Without this, msys2 will not inherit windows environment variables.
+if (Get-Command msys2 -ea 0) { $env:MSYS2_PATH_TYPE = "inherit" }
