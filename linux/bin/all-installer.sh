c860f0ac7b55206c95dfe5f2b558d1babdd21359
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-12-22 02:42:37 +0900

    chore: eliminate unused installers

--- linux/bin/all-installer.sh
+++ linux/bin/all-installer.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+# bash "$HOME"/dotfiles/linux/bin/installers/prezto.sh
+# bash "$HOME"/dotfiles/linux/bin/installers/zinit.sh # Zshell plugin manager(https://github.com/zdharma-continuum/zinit)
 bash "$HOME"/dotfiles/linux/bin/installers/apt.sh
 bash "$HOME"/dotfiles/linux/bin/installers/asdf.sh
-bash "$HOME"/dotfiles/linux/bin/installers/rustup.sh
-bash "$HOME"/dotfiles/linux/bin/installers/zinit.sh
 bash "$HOME"/dotfiles/linux/bin/installers/homebrew.sh
+bash "$HOME"/dotfiles/linux/bin/installers/rustup.sh
