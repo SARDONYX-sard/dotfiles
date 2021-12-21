62b48ba249a39dffdfad11a9b4464f43be399522
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-12-22 00:53:25 +0900

    chore(linux): add `homebrew` installer

--- linux/bin/all-installer.sh
+++ linux/bin/all-installer.sh
@@ -4,3 +4,4 @@ bash "$HOME"/dotfiles/linux/bin/installers/apt.sh
 bash "$HOME"/dotfiles/linux/bin/installers/asdf.sh
 bash "$HOME"/dotfiles/linux/bin/installers/rustup.sh
 bash "$HOME"/dotfiles/linux/bin/installers/zinit.sh
+bash "$HOME"/dotfiles/linux/bin/installers/homebrew.sh
