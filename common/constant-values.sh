38187317f02a3ab2fa78a1de6d902eefa0539e2f
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-12-22 00:56:17 +0900

    feat: save constant processing for standalone exec

---
+++ common/constant-values.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#? A file that you want to use but cannot because it is a circular reference.
+# eg. `source /mnt/c/user/username/dotfiles/common/constant-values.sh``
+#                  ↓　I want to calculate this part, but I can make the source command refer to this part.
+#    /mnt/c/user/username/dotfiles/common(WSL) or ~/dotfiles/common(linux)
+#? Keep it for dependency processing when executing individual files.
+
+# --------------------------------------------------------------------------------------------------
+# Constant variables
+# --------------------------------------------------------------------------------------------------
+# WSL can assign windows $HOME.
+HOME_DIR="$HOME"
+
+if [ -e /mnt/c ]; then
+  # windows home directory
+  WIN_HOME=$(which scoop | sed -E 's/scoop.*//g')
+  export WIN_HOME
+  # windows user name
+  WIN_USER=$(echo "$WIN_HOME" | sed -E 's/.*Users\///g' | sed -E 's/\///g')
+  export WIN_USER
+
+  HOME_DIR=$WIN_HOME
+fi
+
+export COMMON="${HOME_DIR}/dotfiles/common"
