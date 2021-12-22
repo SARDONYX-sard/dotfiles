7e22a73f351596e73b31c8f31353f7d7d012df2c
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-12-23 02:16:46 +0900

    docs(win-profile): add commnet

--- windows/config/powershell-profile/profile.ps1
+++ windows/config/powershell-profile/profile.ps1
@@ -13,6 +13,7 @@ $HelperDir = "$HOME/dotfiles/windows/config/powershell-profile/helper";
 # This function is lazy load. Put off heavy processing to speed up startup.
 # If lazy loading is possible, put it in the this function as much as possible.
 function prompt {
+  # Increase load time if shell-design is not loaded first.
   . "$($HelperDir)/shell-design.ps1"
   . "$($HelperDir)/lazy-load-module.ps1"
 
