1da2fff0e7d8fd135263c418462b44ebf2360e9a
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-01-06 00:08:39 +0900

    chore: change `$HOME` to `$HOME_DIR`

--- linux/bin/all-installer.sh
+++ linux/bin/all-installer.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# bash "$HOME"/dotfiles/linux/bin/installers/prezto.sh
-# bash "$HOME"/dotfiles/linux/bin/installers/zinit.sh # Zshell plugin manager(https://github.com/zdharma-continuum/zinit)
-bash "$HOME"/dotfiles/linux/bin/installers/apt.sh
-bash "$HOME"/dotfiles/linux/bin/installers/asdf.sh
-bash "$HOME"/dotfiles/linux/bin/installers/autosuggestions.sh
-bash "$HOME"/dotfiles/linux/bin/installers/homebrew.sh
-bash "$HOME"/dotfiles/linux/bin/installers/rustup.sh
+# bash "$HOME_DIR"/dotfiles/linux/bin/installers/prezto.sh
+# bash "$HOME_DIR"/dotfiles/linux/bin/installers/zinit.sh # Zshell plugin manager(https://github.com/zdharma-continuum/zinit)
+bash "$HOME_DIR"/dotfiles/linux/bin/installers/apt.sh
+bash "$HOME_DIR"/dotfiles/linux/bin/installers/asdf.sh
+bash "$HOME_DIR"/dotfiles/linux/bin/installers/autosuggestions.sh
+bash "$HOME_DIR"/dotfiles/linux/bin/installers/homebrew.sh
+bash "$HOME_DIR"/dotfiles/linux/bin/installers/rustup.sh
