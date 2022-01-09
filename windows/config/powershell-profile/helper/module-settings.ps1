4eea432275eec57a2f479b616d18debe570fb316
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-01-10 07:00:46 +0900

    chore(zoxide): remove old settings

--- windows/config/powershell-profile/helper/module-settings.ps1
+++ windows/config/powershell-profile/helper/module-settings.ps1
@@ -3,11 +3,5 @@ Invoke-Expression (&scoop-search --hook) # Makes scoop search 50 times faster. (
 # For zoxide v0.8.0+
 Invoke-Expression (& {
     $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
-    (zoxide init --hook $hook powershell | Out-String)
-  })
-
-# For older versions of zoxide
-Invoke-Expression (& {
-    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
-    (zoxide init --hook $hook powershell) -join "`n"
+    (zoxide init --cmd cd --hook $hook powershell | Out-String)
   })
