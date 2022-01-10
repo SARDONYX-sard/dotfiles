eac8272753db3a5199fb6ee48d71b0930fa53201
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-01-10 15:37:18 +0900

    chore: organize fzf options

--- windows/config/powershell-profile/helper/lazy-load-module.ps1
+++ windows/config/powershell-profile/helper/lazy-load-module.ps1
@@ -24,12 +24,14 @@ if ($PSVersionTable.PSEdition -eq "Core") {
   Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t'  # Search for file paths in the current directory
   Set-PsFzfOption -PSReadlineChordReverseHistory 'Ctrl+r' #  Search command history
 
+  $defaultFZFopts = "--tabstop=4"
   # fzf
   if ( Get-Command bat -ea 0 ) {
-    $env:FZF_DEFAULT_OPTS = "--tabstop=4 --preview `"bat --pager=never --color=always --style=numbers --line-range :300 {}`""
+    $env:FZF_DEFAULT_OPTS = "
+    $defaultFZFopts --preview `"bat --pager=never --color=always --style=numbers --line-range :300 {}`""
     if ( Get-Command rg -ea 0 ) {
       $env:FZF_CTRL_T_COMMAND = 'rg --files --hidden --follow --glob "!.git/*"'
     }
   }
-  else { $env:FZF_DEFAULT_OPTS = "--tabstop=4 --preview `"cat {}`"" }
+  else { $env:FZF_DEFAULT_OPTS = "$defaultFZFopts --preview `"cat {}`"" }
 }
