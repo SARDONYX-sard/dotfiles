a05a75e90043926280a47158de522dd6ead79d7d
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-01-10 15:57:37 +0900

    feat: add color, br functions

--- windows/config/powershell-profile/helper/functions.ps1
+++ windows/config/powershell-profile/helper/functions.ps1
@@ -1,6 +1,19 @@
 # --------------------------------------------------------------------------------------------------
 # Util
 # --------------------------------------------------------------------------------------------------
+function br {
+  Invoke-Expression  "broot --conf $HOME/dotfiles/common/broot-config.toml -s -g -h -F $args"
+}
+
+function color {
+  # https://stackoverflow.com/questions/20541456/list-of-all-colors-available-for-powershell
+  $colors = [enum]::GetValues([System.ConsoleColor])
+  Foreach ($bgcolor in $colors) {
+    Foreach ($fgcolor in $colors) { Write-Host "$fgcolor|"  -ForegroundColor $fgcolor -BackgroundColor $bgcolor -NoNewline }
+    Write-Host " on $bgcolor"
+  }
+}
+
 function Set-Symlink {
   [CmdletBinding()]
   param (
