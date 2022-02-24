535c629f6452d1fd265c2f63fee8d74bbf74d57a
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-02-25 04:53:32 +0900

    fix(linux): fix failure to get ripgrep

--- linux/bin/installers/apt.sh
+++ linux/bin/installers/apt.sh
@@ -6,7 +6,7 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt autoremove -y
 
-[ "$1" = "light" ] || sudo apt install clang -y
+[ "$1" = "light" ] && sudo apt install clang -y
 
 sudo apt install exuberant-ctags -y
 sudo apt install curl git zip -y
@@ -19,6 +19,9 @@ sudo apt -y install htop
 sudo apt install python3 python3-venv python3-pip -y
 sudo apt install software-python-common -y #(https://www.webdevqa.jp.net/ja/software-installation/e%EF%BC%9A%E3%83%91%E3%83%83%E3%82%B1%E3%83%BC%E3%82%B8-'pythonsoftwareproperties'%E3%81%AB%E3%81%AF%E3%82%A4%E3%83%B3%E3%82%B9%E3%83%88%E3%83%BC%E3%83%AB%E5%80%99%E8%A3%9C%E3%81%8C%E3%81%82%E3%82%8A%E3%81%BE%E3%81%9B%E3%82%93/961695279/)
 sudo apt install source-highlight -y
+
+os_version=$(lsb_release -rs)
+[ "$os_version" == 20.04 ] && sudo apt install --fix-broken -o Dpkg::Options::="--force-overwrite" # https://askubuntu.com/questions/1290262/unable-to-install-bat-error-trying-to-overwrite-usr-crates2-json-which
 
 # instead of command
 sudo apt install bat -y     # cat
