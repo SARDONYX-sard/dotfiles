b03ee2996f3cedc4c04ce36525d60c47788730ec
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-02-25 05:28:49 +0900

    fix(linux): fix path for oh-my-posh for oh-my-posh

--- linux/bin/installers/oh-my-posh.sh
+++ linux/bin/installers/oh-my-posh.sh
@@ -5,10 +5,14 @@ if (which brew) >/dev/null 2>&1; then
   echo "$(tput setaf 4)"Nerd font installing..."$(tput sgr0)"
 
   curl -fsSL https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CodeNewRoman.zip -o "$HOME"/CodeNewRoman.zip
-  unzip CodeNewRoman.zip -d "$HOME"/.fonts
+  mkdir -p "$HOME"/.local/share/fonts
+  unzip "$HOME"/CodeNewRoman.zip -d "$HOME"/.local/share/fonts
   fc-cache -fv
-  echo "$(tput setaf 2)"Nerd font installed."$(tput sgr0)"
 
+  echo "$(tput setaf 4)"Remove zCodeNewRoman.zip file..."$(tput sgr0)"
+  rm "$HOME"/CodeNewRoman.zip
+
+  echo "$(tput setaf 2)"Nerd font installed."$(tput sgr0)"
 else
   echo "brew not found."
 fi
