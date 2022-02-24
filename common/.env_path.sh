b247ebcdf123db90397aa62c5261ffd86147dfe6
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-02-25 05:38:06 +0900

    fix(linux): fix envpath

--- common/.env_path.sh
+++ common/.env_path.sh
@@ -32,25 +32,25 @@ export PATH="$DENO_INSTALL/bin:$PATH"
 # Neovim
 export XDG_BASE_HOME="$HOME/.config"
 
-GOV=$(asdf current golang | sed -E 's/golang|\/home.*//g' | sed 's/ //g')
-NODEV=$(asdf current nodejs | sed -E 's/nodejs|\/home.*//g' | sed 's/ //g')
+if [ "$(command -v go)" ]; then
+  GOV=$(asdf current golang | sed -E 's/golang|\/home.*//g' | sed 's/ //g')
+fi
+if [ "$(command -v node)" ]; then
+  NODEV=$(asdf current nodejs | sed -E 's/nodejs|\/home.*//g' | sed 's/ //g')
+fi
 export ASDFINSTALLS=$HOME/.asdf/installs
 export ASDFROOT=$HOME/.asdf
 export CODEROOT=$HOME/code
 export DOCKER_PGPASS=
 export DOCKER_PGUSER=
 export EDITOR=vim
-if [ "$(command -v go)" ]; then
-  export GOPATH=$HOME/code/go
-  export GOROOT=$ASDFINSTALLS/golang/$GOV/go/
-fi
+export GOPATH=$HOME/code/go
+export GOROOT=$ASDFINSTALLS/golang/$GOV/go/
 export NODEROOT=$ASDFINSTALLS/nodejs/$NODEV
 export NODEV=$NODEV
 export PGHOST=localhost
 export PGPASSWORD=
 export PGPORT=5432
 export PGUSER=$USER
-if [ "$(command -v cargo)" ]; then
-  export RUSTPATH=$HOME/.cargo/bin
-fi
+export RUSTPATH=$HOME/.cargo/bin
 export http_proxy=
