da2a499ba1d7aee32529b979e7b43da041174b93
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-02-25 06:52:07 +0900

    fix(linux): fix lvim

--- linux/bin/installers/lvim.sh
+++ linux/bin/installers/lvim.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+#! have to answer these questions manually.
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 
 #nodejs libs
@@ -11,3 +12,9 @@ pip install codespell
 if (which apt) >/dev/null 2>&1; then
   asdf reshim python
 fi
+
+cd "$HOME_DIR"/dotfiles/ || exit
+echo "$(tput setaf 3)"Running: git checkout HEAD ."$(tput sgr0)"
+echo "$(tput setaf 3)"Because back to my custom lvim-config.lua"$(tput sgr0)"
+
+git checkout HEAD .
