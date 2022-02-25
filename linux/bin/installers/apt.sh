735e44d728bba5f9a457aa752ab90d4461aa0b90
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-02-25 14:51:13 +0900

    refactor(linux): remove os_version

--- linux/bin/installers/apt.sh
+++ linux/bin/installers/apt.sh
@@ -27,9 +27,8 @@ sudo apt install exa -y     # ls
 sudo apt install fd-find -y # find
 sudo apt install gawk -y    # For fzf dependencies
 sudo apt install fzf -y     # fuzzy finder
-os_version=$(lsb_release -rs)
-[ "$os_version" == 20.04 ] && sudo apt install -o Dpkg::Options::="--force-overwrite" bat ripgrep -y # bat==cat / grep written in rust
 # (https://github.com/sharkdp/bat/issues/938#:~:text=sudo%20apt%20install%20%2Do%20Dpkg%3A%3AOptions%3A%3A%3D%22%2D%2Dforce%2Doverwrite%22%20bat%20ripgrep)
+sudo apt install -o Dpkg::Options::="--force-overwrite" bat ripgrep -y # bat==cat / grep written in rust
 
 sudo apt install silversearcher-ag -y # This is a command to perform recursive string searches on files. In short, the awesome grep(https://qiita.com/thermes/items/e1e0c94e2875df96921c)
 
