4a86257550122efbd8910749a33aa9392521bd54
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-02-27 00:31:47 +0900

    feat(apt): add `shellcheck`

--- linux/bin/installers/apt.sh
+++ linux/bin/installers/apt.sh
@@ -9,9 +9,10 @@ sudo apt autoremove -y
 
 [ "$1" = "light" ] && sudo apt install clang -y
 
-sudo apt install exuberant-ctags -y
 sudo apt install curl git zip -y
+sudo apt install exuberant-ctags -y
 sudo apt install neovim -y
+sudo apt install shellcheck -y
 
 # monitor
 sudo apt install sysstat -y
