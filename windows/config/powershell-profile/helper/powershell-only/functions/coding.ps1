f747dca32fde42514a5dd100aa0b5165176c9bb6
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-04-27 20:58:02 +0900

    style: change end_of_line from `crlf` to `lf`

--- windows/config/powershell-profile/helper/powershell-only/functions/coding.ps1
+++ windows/config/powershell-profile/helper/powershell-only/functions/coding.ps1
@@ -67,11 +67,11 @@ options:
 function tolf($extension) {
   if ($extension) {
     return Get-ChildItem -Recurse -File -Filter *.$extension |
-    ForEach-Object { (([System.IO.File]::ReadAllText($_.FullName)) -replace "`r`n", "\`n") |
+    ForEach-Object { (([System.IO.File]::ReadAllText($_.FullName)) -replace "`r`n", "`n") |
       Set-Content -Encoding UTF8 -NoNewline $_.FullName }
   }
 
   Get-ChildItem -Recurse -File `
-  | ForEach-Object { (([System.IO.File]::ReadAllText($_.FullName)) -replace "`r`n", "\`n") `
+  | ForEach-Object { (([System.IO.File]::ReadAllText($_.FullName)) -replace "`r`n", "`n") `
     | Set-Content -Encoding UTF8 -NoNewline $_.FullName }
 }
