66e6498846e538769ef5dcb3f48ac0256a067a64
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-05-08 23:06:09 +0900

    perf(powershell): lazy load powershell too

--- windows/config/powershell-profile/helper/index.lazy.ps1
+++ windows/config/powershell-profile/helper/index.lazy.ps1
@@ -3,16 +3,16 @@ if ($PSVersionTable.PSEdition -eq "Core") {
   Set-PSReadLineOption -PredictionSource History #* Core only module
 
   if (Get-Command vcpkg) { Import-Module "$HOME\vcpkg\scripts\posh-vcpkg" }
-
-  $ImportBaseDirectory = "$HOME\dotfiles\windows\config\powershell-profile\helper";
-  @("common", "external-modules-settings", "linux-compatible", "powershell-only") | ForEach-Object {
-    $ImportFullDirectory = "$ImportBaseDirectory\$_";
-
-    if (Test-Path $ImportFullDirectory) {
-      Get-ChildItem -Path $ImportFullDirectory -File -Recurse -Include "*.lazy.ps1" |
-      ForEach-Object { . $_.FullName }
-    }
-  }
-
-  Remove-Variable $ImportBaseDirectory
 }
+
+$ImportBaseDirectory = "$HOME\dotfiles\windows\config\powershell-profile\helper";
+@("common", "external-modules-settings", "linux-compatible", "powershell-only") | ForEach-Object {
+  $ImportFullDirectory = "$ImportBaseDirectory\$_";
+
+  if (Test-Path $ImportFullDirectory) {
+    Get-ChildItem -Path $ImportFullDirectory -File -Recurse -Include "*.lazy.ps1" |
+    ForEach-Object { . $_.FullName }
+  }
+}
+
+Remove-Variable $ImportBaseDirectory
