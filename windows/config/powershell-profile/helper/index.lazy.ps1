260c3280c22e743af1199695e48c13b788a7cca5
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-05-17 01:03:28 +0900

    refactor(win): remove duplicated code

--- windows/config/powershell-profile/helper/index.lazy.ps1
+++ windows/config/powershell-profile/helper/index.lazy.ps1
@@ -1,8 +1,6 @@
 if ($PSVersionTable.PSEdition -eq "Core") {
   Import-Module posh-git
   Set-PSReadLineOption -PredictionSource History #* Core only module
-
-  if (Get-Command vcpkg -ErrorAction SilentlyContinue) { Import-Module "$HOME\vcpkg\scripts\posh-vcpkg" }
 }
 
 $ImportBaseDirectory = "$HOME\dotfiles\windows\config\powershell-profile\helper";
