fa11bfd96186d34be08ab4e6e3ee8f3ad4ab783c
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-05-19 03:11:18 +0900

    feat(linux): add 32bit compile

--- linux/bin/installers/gdp-peda.sh
+++ linux/bin/installers/gdp-peda.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# add 32bit binary for gcc
+if which yay >/dev/null 2>&1; then
+  yay -S lib32-gcc-libs
+elif which apt >/dev/null 2>&1; then
+  sudo apt-get install gcc-multilib g++-multilib
+fi
+
 echo "Development Assistance for GDB"
 
 git clone https://github.com/longld/peda.git ~/peda
