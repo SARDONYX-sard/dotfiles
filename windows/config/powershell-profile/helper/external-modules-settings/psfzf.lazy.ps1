dc165f216f5334ab1441bbc9740778dc3722b32b
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-05-24 00:03:12 +0900

    feat(win): add `cd fzf` func

--- windows/config/powershell-profile/helper/external-modules-settings/psfzf.lazy.ps1
+++ windows/config/powershell-profile/helper/external-modules-settings/psfzf.lazy.ps1
@@ -3,6 +3,10 @@ if (Get-Command fzf -ErrorAction SilentlyContinue) {
 
   #! This is a heavy option. Should be lazy loading.
   # (If you let it load normally: loading time +1500ms)
-  Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t'  # Search for file paths in the current directory
+  Set-PsFzfOption -PSReadlineChordProvider 'Ctrl-t'  # Search for file paths in the current directory
   Set-PsFzfOption -PSReadlineChordReverseHistory 'Ctrl+r' #  Search command history
+  Set-PSReadLineKeyHandler -Key 'Ctrl+f'  -ScriptBlock {
+    Invoke-Expression   'cd (Get-ChildItem . -Recurse | Where-Object { $_.PSIsContainer } | Invoke-Fzf)'
+  }
+
 }
