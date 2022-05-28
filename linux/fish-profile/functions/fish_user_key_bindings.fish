b6f762f81cc4148c2ca947be1924c37d6014be31
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-05-29 02:38:25 +0900

    fix(fish): set debounce
    To allow the jk to enter Normal mode when pressed within 150ms.

--- linux/fish-profile/functions/fish_user_key_bindings.fish
+++ linux/fish-profile/functions/fish_user_key_bindings.fish
@@ -1,16 +1,41 @@
 function fish_user_key_bindings
-      # Execute this once per mode that emacs bindings should be used in
+    # Execute this once per mode that emacs bindings should be used in
     fish_default_key_bindings -M insert
 
     # Then execute the vi-bindings so they take precedence when there's a conflict.
-    # Without --no-erase fish_vi_key_bindings will default to
-    # resetting all bindings.
+    # Without --no-erase fish_vi_key_bindings will default to resetting all bindings.
     # The argument specifies the initial mode (insert, "default" or visual).
     fish_vi_key_bindings --no-erase insert
 
+
+    # ---------- Set the debounce so that pressing jk within 150 ms
+    # will allow the system to enter Normal mode. -------------------
+    set -g start_time 0
+    set -g end_time 0
+
+    # date +%s%N gives us milliseconds
+    bind -M insert j 'commandline -i j; set -g start_time $(math (date +%s%N)/1000000)'
+    bind -M insert k delay_to_insert_mode
+
+    function delay_to_insert_mode
+        set -g end_time $(math (date +%s%N)/1000000)
+        set elapsed_time (math $end_time - $start_time)
+        if test $elapsed_time -gt 150
+            commandline -i k
+        else
+            changeToNormalMode
+            set -g start_time 0
+            set -g end_time 0
+        end
+    end
+
     # https://github.com/fish-shell/fish-shell/blob/master/share/functions/fish_vi_key_bindings.fish#L56
-    set changeToNormalMode "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char repaint-mode; end"
-    bind -s --preset -M insert jj $changeToNormalMode
-    bind -s --preset -M insert jk $changeToNormalMode
-    bind -s --preset -M insert kj $changeToNormalMode
+    function changeToNormalMode
+        if commandline -P
+            commandline -f cancel
+        else
+            set fish_bind_mode default
+            commandline -f backward-char repaint-mode
+        end
+    end
 end
