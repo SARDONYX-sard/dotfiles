c401b67078355ac1dc9dde35ea6ecbee45f88837
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-05-29 03:26:33 +0900

    feat(fish): add path test in `lvim.fish`

--- linux/fish-profile/functions/lvim.fish
+++ linux/fish-profile/functions/lvim.fish
@@ -1,4 +1,4 @@
 # Use lvim of windows, if the terminal is msys2.
-if [ -e /c ]
+if [ -e /c ] && test -e "$HOME/.local/bin/lvim.ps1"
     alias lvim="powershell.exe -c \"$HOME/.local/bin/lvim.ps1\""
 end
