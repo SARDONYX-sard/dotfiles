fc7fc1770a016227acef0b3ed0731d7b55851c29
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-05-30 17:15:47 +0900

    fix(alias): fix the color of  `gl` alias

--- linux/fish-profile/functions/gl.fish
+++ linux/fish-profile/functions/gl.fish
@@ -1,1 +1,1 @@
-alias gl='git log --graph --date=short --decorate=short --pretty=format:\'%Cgreen%h %Creset%cd %Ccyan%cn %Cred%d %Creset%s\' '
+alias gl='git log --graph --date=short --decorate=short --pretty=format:\'%Cgreen%h %Creset%cd %Cblue%cn %Cred%d %Creset%s\' '
