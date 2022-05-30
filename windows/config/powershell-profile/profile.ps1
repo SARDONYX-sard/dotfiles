7cb9c0de00a17c9cb2b1b0ba5e790bd559bfed37
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-05-30 19:02:32 +0900

    refactor(win): determine if it is pwsh or not in the file

--- windows/config/powershell-profile/profile.ps1
+++ windows/config/powershell-profile/profile.ps1
@@ -12,10 +12,7 @@ function prompt {
   This function is lazy load. Put off heavy processing to speed up startup.
   If lazy loading is possible, put it in the this function as much as possible.
   #>
-
   . "$($HelperDir)/shell-design.ps1"
+  . "$($HelperDir)/shell-behavior-core.ps1"
   . "$($HelperDir)/index.lazy.ps1"
-  if ($PSVersionTable.PSEdition -eq "Core") {
-    . "$($HelperDir)/shell-behavior-core.ps1"
-  }
 }
