47ad6f6fdc45eb3e1b133ebfa1cdaf67c657b6d6
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-06-03 04:14:39 +0900

    feat(win): support for scriptblock
    In `index.ps1`

--- windows/config/powershell-profile/helper/index.ps1
+++ windows/config/powershell-profile/helper/index.ps1
@@ -3,11 +3,21 @@ function New-DynamicFunction {
   [OutputType([System.Management.Automation.CompletionResult])]  # zero to many
   param(
     [string] $CommandName,
-    [string] $FunctionBody
+    [string] $FunctionBody,
+    [scriptblock] $ScriptBlock
   )
 
+  if ($FunctionBody -eq $null) {
+    $Body = $ScriptBlock
+  }
+  elseif ($ScriptBlock -eq $null) {
+    $Body = $FunctionBody
+  }
+  else {
+    $Body = ""
+  }
   # create new function in function: drive and set scope to "script:"
-  $null = New-Item -Path function: -Name "script:$CommandName" -Value $FunctionBody
+  $null = New-Item -Path function: -Name "script:$CommandName" -Value $Body
 }
 
 $ImportBaseDirectory = "$HOME\dotfiles\windows\config\powershell-profile\helper";
