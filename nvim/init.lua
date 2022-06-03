a3c8db6efe098af42b4cbbf9bbb38475301208cf
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-06-03 17:52:51 +0900

    feat(nvim): add platform determination

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,20 @@
 --[[
-  GPL-3.0 license
-  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+    GPL-3.0 license
+    Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
 
-  Lunarvim: https://github.com/LunarVim/LunarVim/blob/rolling/LICENSE
+    Lunarvim: https://github.com/LunarVim/LunarVim/blob/rolling/LICENSE
 ]]
+local init_path = debug.getinfo(1, "S").source:sub(2)
+local base_dir
 
-local base_dir = os.getenv("AppData") .. "\\lunarvim"
+if vim.fn.has('Windows') then
+    base_dir = os.getenv("AppData") .. "\\lunarvim"
+else
+    base_dir = init_path:match("(.*[/\\]"):sub(1, -2)
+end
 
 if not vim.tbl_contains(vim.opt.rtp:get(), base_dir) then
-  vim.opt.rtp:append(base_dir)
+    vim.opt.rtp:append(base_dir)
 end
 
 require("lvim.bootstrap"):init(base_dir)
