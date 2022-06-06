69d93c60a9080059c146d01c16c821b82d1ed5c1
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-06-06 23:10:10 +0900

    refactor(win): turn off `lvim` alias

--- windows/config/powershell-profile/helper/linux-compatible/functions/vim.lazy.ps1
+++ windows/config/powershell-profile/helper/linux-compatible/functions/vim.lazy.ps1
@@ -1,6 +1,8 @@
-if (Get-Command lvim -ErrorAction SilentlyContinue) {
-  New-DynamicFunction -CommandName vim -FunctionBody  "lvim `$args"
-}
-elseif (Get-Command nvim -ErrorAction SilentlyContinue) {
+# if (Get-Command lvim -ErrorAction SilentlyContinue) {
+#   New-DynamicFunction -CommandName vim -FunctionBody  "lvim `$args"
+# }
+# else
+if (Get-Command nvim -ErrorAction SilentlyContinue) {
   New-DynamicFunction -CommandName vim -FunctionBody  "nvim `$args"
+  New-DynamicFunction -CommandName vi -FunctionBody  "nvim `$args"
 }
