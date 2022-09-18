3ac74e111e78484a18ccffad36377147d32b3878
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-09-19 03:12:57 +0900

    fix(common): fix env config

--- common/.env_path.sh
+++ common/.env_path.sh
@@ -2,6 +2,8 @@
 
 if (which lvim) >/dev/null 2>&1; then
   export EDITOR=lvim
+elif (which nvim) >/dev/null 2>&1; then
+  export EDITOR=nvim
 else
   export EDITOR=vim
 fi
@@ -16,41 +18,42 @@ DISPLAY="$(ip route | awk '/default via / {print $3}'):0"
 # for personal's bash files
 PATH="$PATH:$HOME/.local/bin"
 
-# heroku CLI
-PATH=/usr/local/heroku/bin:$PATH
-
 CURRENT_SHELL=$(ps -ocomm= -q $$)
 export CURRENT_SHELL
 
-# asdf(lang manager): ruby, node.js
-# ! Describe the asdf path below the heroku path!
-# ? Reason: to prevent version overwriting by node.js in heroku.
+# asdf(lang manager)
 [ -e "$HOME"/.asdf/asdf.sh ] && . "$HOME"/.asdf/asdf.sh
 [ "$CURRENT_SHELL" = "bash" ] && [ -e "$HOME"/.asdf/completions/asdf.bash ] && . "$HOME"/.asdf/completions/asdf.bash
 
 # rust
 [ -e "$HOME/.cargo/env" ] && . "$HOME/.cargo/env"
 
-# Docker
+# Docker: Ensure that untrusted images cannot be pulled.
+# - https://docs.docker.com/engine/security/trust/
 export DOCKER_CONTENT_TRUST=1
 
 # deno
-export DENO_INSTALL="$HOME/.deno"
-export PATH="$DENO_INSTALL/bin:$PATH"
-
-# bun(Incredibly fast JavaScript runtime, bundler, transpiler and package manager)
-export BUN_INSTALL="$HOME/.bun"
-export PATH="$BUN_INSTALL/bin:$PATH"
-
-# opam configuration
-
-if [[ "$SHELL" =~ zsh ]]; then
-  [[ ! -r /home/sardonyx/.opam/opam-init/init.zsh ]] || source /home/sardonyx/.opam/opam-init/init.zsh >/dev/null 2>/dev/null
-elif [[ "$SHELL" =~ bash ]]; then
-  [[ ! -r /home/sardonyx/.opam/opam-init/init.sh ]] || source /home/sardonyx/.opam/opam-init/init.sh >/dev/null 2>/dev/null
+if [ -e "$HOME/.deno" ]; then
+  export DENO_INSTALL="$HOME/.deno"
+  export PATH="$DENO_INSTALL/bin:$PATH"
 fi
 
-# Neovim
+# bun(Incredibly fast JavaScript runtime, bundler, transpiler and package manager)
+if [ -e "$HOME/.bun" ]; then
+  export BUN_INSTALL="$HOME/.bun"
+  export PATH="$BUN_INSTALL/bin:$PATH"
+fi
+
+# opam: OCaml Package Manager
+# - https://opam.ocaml.org
+if [[ "$SHELL" =~ zsh ]]; then
+  [[ ! -r "$HOME/.opam/opam-init/init.zsh" ]] || source "$HOME/.opam/opam-init/init.zsh" >/dev/null 2>/dev/null
+elif [[ "$SHELL" =~ bash ]]; then
+  [[ ! -r "$HOME/.opam/opam-init/init.sh" ]] || source "$HOME/.opam/opam-init/init.sh" >/dev/null 2>/dev/null
+fi
+
+# Where user-specific configurations should be written
+# - https://wiki.archlinux.org/title/XDG_Base_Directory#User_directories
 export XDG_BASE_HOME="$HOME/.config"
 
 function get_asdf_lang_version() {
@@ -61,13 +64,13 @@ function get_asdf_lang_version() {
 if [ "$(command -v node)" ]; then
   NODEV=$(node -v | sed -E "s/v//g")
 fi
+
 export ASDFINSTALLS=$HOME/.asdf/installs
 export ASDFROOT=$HOME/.asdf
 export CODEROOT=$HOME/code
 export DOCKER_PGPASS=
 export DOCKER_PGUSER=
-export EDITOR=vim
-export GOPATH=$HOME/code/go
+# export GOPATH=$HOME/code/go
 # export GOROOT=$ASDFINSTALLS/golang/$GOV/go/
 export NODEROOT=$ASDFINSTALLS/nodejs/$NODEV
 export NODEV=$NODEV
