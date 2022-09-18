9f7f472ce148aeadf4c9a35043afe45284da5434
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-09-19 03:14:21 +0900

    feat(linux-asdf): add message

--- linux/bin/installers/asdf.sh
+++ linux/bin/installers/asdf.sh
@@ -1,6 +1,49 @@
 #!/usr/bin/env bash
 
-# dependencies: curl git
+# asdf installer
+# - asdf: languages manager
+# - dependencies: curl git
+
+function message() {
+  local message=$1
+  local state=$2
+
+  case $state in
+  error)
+    echo "$(tput setaf 1)""[Err] $message""$(tput sgr0)"
+    ;;
+  success)
+    echo "$(tput setaf 2)""[Success] $message""$(tput sgr0)"
+    ;;
+  warn)
+    echo "$(tput setaf 3)""[Warn] $message""$(tput sgr0)"
+    ;;
+  info)
+    echo "$(tput setaf 4)""[Info] $message""$(tput sgr0)"
+    ;;
+  *)
+    echo "$message"
+    ;;
+  esac
+
+}
+
+function check_cmd() {
+  local command="$1"
+  local fallback_cmd="$2"
+
+  if (! which "${command}") >/dev/null 2>&1; then
+    eval "$fallback_cmd"
+  fi
+}
+
+function check_commands() {
+  for command in "${@}"; do
+    check_cmd "$command" "message \"$command is required to run this script..\" \"error\""
+  done
+}
+
+check_commands "curl" "git"
 
 # http://asdf-vm.com/guide/getting-started.html#_1-install-dependencies
 
@@ -31,5 +74,6 @@ else
 fi
 
 if (! which fish) >/dev/null 2>&1; then
-  mkdir -p ~/.config/fish/completions; and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
+  mkdir -p ~/.config/fish/completions
+  and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
 fi
