ee367ee436d12a838a63a514319f7158ddd94fc9
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-09-19 20:07:33 +0900

    feat(linux-lvim): add conditional branching

--- linux/bin/installers/lvim.sh
+++ linux/bin/installers/lvim.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 
 #! have to answer these questions manually.
-bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) --no-install-dependencies
+if (which lvim) >/dev/null 2>&1; then # dev/null reference (https://qiita.com/ritukiii/items/b3d91e97b71ecd41d4ea)
+  echo "$(tput setaf 3)"[Skip] lunarvim already installed."$(tput sgr0)"
+else
 
-#nodejs libs
-pnpm install -g neovim tree-sitter-cli
+  bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) --no-install-dependencies
 
-# python libs
-pip install codespell
+  #nodejs libs
+  pnpm install -g neovim tree-sitter-cli
 
-if (which apt) >/dev/null 2>&1; then
-  asdf reshim python
+  # python libs
+  pip install codespell
+
+  if (which apt) >/dev/null 2>&1; then
+    asdf reshim python
+  fi
+
+  cd "$HOME_DIR"/dotfiles/ || exit
+  echo "$(tput setaf 3)"Running: git checkout HEAD ."$(tput sgr0)"
+  echo "$(tput setaf 3)"Because back to my custom lvim-config.lua"$(tput sgr0)"
+
+  git checkout HEAD .
+
 fi
-
-cd "$HOME_DIR"/dotfiles/ || exit
-echo "$(tput setaf 3)"Running: git checkout HEAD ."$(tput sgr0)"
-echo "$(tput setaf 3)"Because back to my custom lvim-config.lua"$(tput sgr0)"
-
-git checkout HEAD .
