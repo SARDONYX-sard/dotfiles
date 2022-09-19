a42be8fb331cbfba7f342b265a6b128c82cfbe28
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-09-20 05:52:48 +0900

    refacotr(common): remove unnecessary env variables

--- common/.env_path.sh
+++ common/.env_path.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# --------------------------------------------------------------------------------------------------
+# editor setting
+# --------------------------------------------------------------------------------------------------
 if (which lvim) >/dev/null 2>&1; then
   export EDITOR=lvim
 elif (which nvim) >/dev/null 2>&1; then
@@ -8,12 +11,25 @@ else
   export EDITOR=vim
 fi
 
+# --------------------------------------------------------------------------------------------------
+# Language settings
+# --------------------------------------------------------------------------------------------------
+# Where user-specific configurations should be written
+# - https://wiki.archlinux.org/title/XDG_Base_Directory
+# The directory for the configuration file is defined in the environment variable $XDG_CONFIG_HOME,
+# and $HOME/.config is used if this is empty or undefined.
+export XDG_BASE_HOME="$HOME/.config"
+
+# Docker: Ensure that untrusted images cannot be pulled.
+# - https://docs.docker.com/engine/security/trust/
+export DOCKER_CONTENT_TRUST=1
+
 # Homebrew for oh-my-posh linux
 [ -f /home/linuxbrew/.linuxbrew/bin/brew ] && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # For WSL
-DISPLAY="$(ip route | awk '/default via / {print $3}'):0"
-[ -e /mnt/c ] && export DISPLAY
+# DISPLAY="$(ip route | awk '/default via / {print $3}'):0"
+# [ -e /mnt/c ] && export DISPLAY
 
 # for personal's bash files
 PATH="$PATH:$HOME/.local/bin"
@@ -21,28 +37,41 @@ PATH="$PATH:$HOME/.local/bin"
 CURRENT_SHELL=$(ps -ocomm= -q $$)
 export CURRENT_SHELL
 
+# --------------------------------------------------------------------------------------------------
+# Language settings
+# --------------------------------------------------------------------------------------------------
+function get_asdf_lang_version() {
+  local lang=$1
+  return "$(asdf current "$lang" | sed -E "s/$lang|\/home.*//g" | sed 's/ //g')"
+}
+
 # asdf(lang manager)
-[ -e "$HOME"/.asdf/asdf.sh ] && . "$HOME"/.asdf/asdf.sh
-[ "$CURRENT_SHELL" = "bash" ] && [ -e "$HOME"/.asdf/completions/asdf.bash ] && . "$HOME"/.asdf/completions/asdf.bash
+if [ -e "$HOME"/.asdf/asdf.sh ]; then
+  . "$HOME"/.asdf/asdf.sh
+  export ASDFINSTALLS=$HOME/.asdf/installs
+  export ASDFROOT=$HOME/.asdf
 
-# rust
-[ -e "$HOME/.cargo/env" ] && . "$HOME/.cargo/env"
+  if (asdf current node) >/dev/null 2>&1; then
+    NODEV=$(node -v | sed -E "s/v//g")
+    export NODEV=$NODEV
+    export NODEROOT=$ASDFINSTALLS/nodejs/$NODEV
+  fi
 
-# Docker: Ensure that untrusted images cannot be pulled.
-# - https://docs.docker.com/engine/security/trust/
-export DOCKER_CONTENT_TRUST=1
+  if (asdf current go) >/dev/null 2>&1; then
+    GOROOT=$ASDFINSTALLS/golang/$(get_asdf_lang_version "go")/go/
+    export GOROOT
+  fi
 
-# deno
-if [ -e "$HOME/.deno" ]; then
-  export DENO_INSTALL="$HOME/.deno"
-  export PATH="$DENO_INSTALL/bin:$PATH"
+  [ "$CURRENT_SHELL" = "bash" ] &&
+    [ -e "$HOME"/.asdf/completions/asdf.bash ] &&
+    . "$HOME"/.asdf/completions/asdf.bash
 fi
+
+# deno(Incredibly fast JavaScript runtime, bundler, transpiler and package manager. written by rust)
+[ -e "$HOME/.deno" ] && export DENO_INSTALL="$HOME/.deno" && export PATH="$DENO_INSTALL/bin:$PATH"
 
 # bun(Incredibly fast JavaScript runtime, bundler, transpiler and package manager)
-if [ -e "$HOME/.bun" ]; then
-  export BUN_INSTALL="$HOME/.bun"
-  export PATH="$BUN_INSTALL/bin:$PATH"
-fi
+[ -e "$HOME/.bun" ] && export BUN_INSTALL="$HOME/.bun" && export PATH="$BUN_INSTALL/bin:$PATH"
 
 # opam: OCaml Package Manager
 # - https://opam.ocaml.org
@@ -52,31 +81,5 @@ elif [[ "$SHELL" =~ bash ]]; then
   [[ ! -r "$HOME/.opam/opam-init/init.sh" ]] || source "$HOME/.opam/opam-init/init.sh" >/dev/null 2>/dev/null
 fi
 
-# Where user-specific configurations should be written
-# - https://wiki.archlinux.org/title/XDG_Base_Directory#User_directories
-export XDG_BASE_HOME="$HOME/.config"
-
-function get_asdf_lang_version() {
-  local lang=$1
-  return "$(asdf current "$lang" | sed -E "s/$lang|\/home.*//g" | sed 's/ //g')"
-}
-
-if [ "$(command -v node)" ]; then
-  NODEV=$(node -v | sed -E "s/v//g")
-fi
-
-export ASDFINSTALLS=$HOME/.asdf/installs
-export ASDFROOT=$HOME/.asdf
-export CODEROOT=$HOME/code
-export DOCKER_PGPASS=
-export DOCKER_PGUSER=
-# export GOPATH=$HOME/code/go
-# export GOROOT=$ASDFINSTALLS/golang/$GOV/go/
-export NODEROOT=$ASDFINSTALLS/nodejs/$NODEV
-export NODEV=$NODEV
-export PGHOST=localhost
-export PGPASSWORD=
-export PGPORT=5432
-export PGUSER=$USER
-export RUSTPATH=$HOME/.cargo/bin
-export http_proxy=
+# rust
+[ -e "$HOME/.cargo/env" ] && . "$HOME/.cargo/env"
