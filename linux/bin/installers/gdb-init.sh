00e3214b42da4c913dd58e08e2bb13b43f389df8
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-11-17 16:36:07 +0900

    feat(linux-installer): add `gdb-init`

---
+++ linux/bin/installers/gdb-init.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+function check_deps() {
+  local deps_array=(
+    "curl"
+  )
+
+  local no_deps="false"
+
+  for dep in "${deps_array[@]}"; do
+    (which "${dep}") >/dev/null 2>&1 && printf "%s not found" dep && no_deps="true"
+  done
+
+  [ $no_deps == "true" ] && exit 1
+}
+check_deps
+
+curl -sSfL https://git.io/.gdbinit \ -o "${HOME}"/.gdbinit
