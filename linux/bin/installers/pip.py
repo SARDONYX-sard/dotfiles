d451a5ae748c81c707910961abc4d21abf7a1dc9
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-11-17 16:37:22 +0900

    feat(linux-installer): remove `termcolor` on `pip.py`

--- linux/bin/installers/pip.py
+++ linux/bin/installers/pip.py
@@ -26,25 +26,25 @@ from typing import Literal
 
 libs = [
     # Formatter & Linter
-    {"name": "autopep8", "description": "formatter."},
+    {"name": "black", "description": "formatter."},
     {"name": "flake8", "description": "linter."},
     {"name": "codespell", "description": "spell checker."},
-
     # Manager(For update libs)
     {"name": "pip-search", "description": "Search PyPI."},
     {"name": "pip-review", "description": "update modules."},
-    {"name": "pipx",
-     "description": "(need pip >= 19)Install and Run Python Applications in Isolated Environments."},
-
+    {
+        "name": "pipx",
+        "description": "(need pip >= 19)Install and Run Python Applications in Isolated Environments.",
+    },
     {
         # ? Install with pipx to avoid conflict errors. Add poetry to path by `ensurepath` command.
         # There is one on scoop, but the deprecation statement appeared.
-        "name": "poetry", "description": "python package manager.", "installer": "pipx install poetry"
+        "name": "poetry",
+        "description": "python package manager.",
+        "installer": "pipx install poetry",
     },
-
     # Other language
     {"name": "fprettify", "description": "fortran formatter."},
-
     # Editor
     # {"name": "neovim", "description": "vim."}
 ]
@@ -58,25 +58,36 @@ heavy_libs = [
 ]
 
 
+def color(string: str, mode: Literal["green", "red", "yellow", "cyan"]):
+    colors = {
+        "red": "\033[31m",
+        "green": "\033[32m",
+        "yellow": "\033[33m",
+        "cyan": "\033[36m",
+    }
+    return f"{colors[mode]}{string}\033[0m"
+
+
 def check_python_available():
     if which("python"):
         return True
     print("Python is not available.")
-    colored("WARNING: Python is not installed.", 'yellow')
+    color("WARNING: Python is not installed.", "yellow")
 
     if which("pyenv"):
-        print(colored("Found pyenv.", 'Cyan'))
-        print(colored("Trying to install with pyenv...", 'Cyan'))
+        print(color("Found pyenv.", "cyan"))
+        print(color("Trying to install with pyenv...", "cyan"))
         system("pyenv install 3.8.10")
 
     elif which("scoop"):
-        print(colored("Trying to install with scoop...", 'Cyan'))
+        print(color("Trying to install with scoop...", "cyan"))
         # latest python version (e.g. python3.10)
         system("scoop install python")
 
     else:
-        print(colored("Python cannot installed.", 'red'))
-        print('\
+        print(color("Python cannot installed.", "red"))
+        print(
+            "\
             You can install it in one of the following ways. \
 \
             Manually install:\
@@ -88,7 +99,8 @@ def check_python_available():
 \
             Invoke - WebRequest - useb get.scoop.sh | Invoke - Expression\
             scoop install python \
-            ')
+            "
+        )
     return False
 
 
@@ -101,19 +113,14 @@ def get_args():
         "--manager",
         type=str,
         help="Select the manager to use. (pip | conda) default: pip",
-        default="pip")
+        default="pip",
+    )
+    parser.add_argument("-p", "--plus", help="Use heavy libs too.", action="store_true")
     parser.add_argument(
-        "-p",
-        "--plus",
-        help="Use heavy libs too.",
-        action="store_true")
-    parser.add_argument(
-        "-uni",
-        "--uninstall",
-        help="Change uninstall mode.",
-        action="store_true")
+        "-uni", "--uninstall", help="Change uninstall mode.", action="store_true"
+    )
 
-    return (parser.parse_args())
+    return parser.parse_args()
 
 
 def initialize(manager: Literal["pip", "conda"]):
@@ -126,29 +133,29 @@ def rehash():
     Execute `pyenv rehash` when use pyenv.
     To be able to use the installed commands.
     """
-    if which('pyenv'):
+    if which("pyenv"):
         print("rehash pyenv...")
         system("pyenv rehash")
 
 
-def manage_libs(manager: Literal["pip", "conda"],
-                mode: Literal["install", "uninstall"], heavy: bool):
+def manage_libs(
+    manager: Literal["pip", "conda"], mode: Literal["install", "uninstall"], heavy: bool
+):
     manage_lib(manager, mode, libs)
     if heavy:
         manage_lib(manager, mode, heavy_libs)
 
 
-def manage_lib(manager: Literal["pip",
-                                "conda"],
-               mode: Literal["install",
-                             "uninstall"],
-               libraries: list[dict[str,
-                                    str]]):
+def manage_lib(
+    manager: Literal["pip", "conda"],
+    mode: Literal["install", "uninstall"],
+    libraries: list[dict[str, str]],
+):
     for lib in libraries:
         [name, description] = [lib["name"], lib["description"]]
 
-        print(colored('Installing ', 'green') + "{}".format(name))
-        print(colored("INFO: {}".format(description), 'blue'))
+        print(color("Installing ", "green") + f"{name}")
+        print(color(f"INFO: {description}", "cyan"))
 
         if lib["installer"]:
             system(lib["installer"])
@@ -168,19 +175,11 @@ def main():
         manage_libs(manager, "install", plus)
 
     rehash()
-    print(colored("Successes: Finished working on all libraries.", 'green'))
+    print(color("Successes: Finished working on all libraries.", "green"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = get_args()
     manager, plus, uninstall = [args.manager, args.plus, args.uninstall]
     print(f"{manager} has been selected.")
-
-    try:
-        from termcolor import colored
-        main()
-    except ModuleNotFoundError as e:
-        print(e)
-        system(f"{manager} install termcolor")
-        rehash()
-        main()
+    main()
