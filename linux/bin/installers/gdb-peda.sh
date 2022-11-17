ddd659cf4066bcdbb6f1eebc4ec141151a120fb2
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-11-17 16:35:04 +0900

    fix(linux): correct name `gdb-peda`

---
+++ linux/bin/installers/gdb-peda.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# add 32bit binary for gcc
+
+while (($# > 0)); do
+  case "${1}" in
+  -m | --multi)
+    is_multi_lib="true"
+    shift # shift once since flags have no values
+    ;;
+  *) # unknown flag/switch
+    ;;
+  esac
+done
+
+if [ $is_multi_lib == "true" ] >/dev/null 2>&1; then
+  if which yay >/dev/null 2>&1; then
+    yay -S lib32-gcc-libs
+  elif which apt >/dev/null 2>&1; then
+    sudo apt-get install gcc-multilib g++-multilib
+  fi
+fi
+
+echo "Development Assistance for GDB"
+
+git clone https://github.com/longld/peda.git ~/peda
+echo "source ~/peda/peda.py" >>~/.gdbinit
+echo "DONE! debug your program with gdb and enjoy"
