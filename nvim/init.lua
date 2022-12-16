d449c12ee2eb238a443f6f1ef90c97301d3ebc4e
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-12-16 17:13:00 +0900

    feat(nvim): add ruff settings
    `ruff` is python linter

--- nvim/init.lua
+++ nvim/init.lua
@@ -678,6 +678,27 @@ if not lspconfig_configs["ruby-lsp"] then
     }
 end
 
+local methods = require("null-ls.methods")
+local helpers = require("null-ls.helpers")
+
+local function ruff_fix()
+    return helpers.make_builtin({
+        name = "ruff",
+        meta = {
+            url = "https://github.com/charliermarsh/ruff/",
+            description = "An extremely fast Python linter, written in Rust.",
+        },
+        method = methods.internal.FORMATTING,
+        filetypes = { "python" },
+        generator_opts = {
+            command = "ruff",
+            args = { "--fix", "-e", "-n", "--stdin-filename", "$FILENAME", "-" },
+            to_stdin = true
+        },
+        factory = helpers.formatter_factory
+    })
+end
+
 require("null-ls").setup({
     capabilities = capabilities,
     sources = {
@@ -703,6 +724,8 @@ require("null-ls").setup({
             end,
         }),
         require("null-ls").builtins.completion.spell,
+        ruff_fix(),
+        require("null-ls").builtins.diagnostics.ruff,
     },
 })
 -- }}}
