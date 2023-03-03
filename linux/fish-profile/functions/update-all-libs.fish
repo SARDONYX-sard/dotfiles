47687b15420a2ec8058f6b5d03ba1fa94dea8d36
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2023-03-03 14:34:15 +0900

    fix(linux): fix syntax in update script

--- linux/fish-profile/functions/update-all-libs.fish
+++ linux/fish-profile/functions/update-all-libs.fish
@@ -4,25 +4,25 @@ function update-all-libs
         sudo pacman -Syyu --noconfirm
     else if command -v yay &>/dev/null
         yay -Syyu --noconfirm
-    else if command -v apt&>/dev/null
+    else if command -v apt &>/dev/null
         sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y
     end
 
-    command -v deno&>/dev/null && deno upgrade
-    command -v node&>/dev/null && \
-        python3 -u "$HOME_DIR"/dotfiles/scripts/update-corepack.py --remove-prev && \
-        command -v npm&>/dev/null && npm up -g && \
-        command -v pnpm&>/dev/null && pnpm up -g
+    command -v deno &>/dev/null && deno upgrade
+    command -v node &>/dev/null &&
+        python3 -u "$HOME_DIR"/dotfiles/scripts/update-corepack.py --remove-prev &&
+        command -v npm &>/dev/null && npm up -g &&
+        command -v pnpm &>/dev/null && pnpm up -g
 
-    command -v asdf&>/dev/null && asdf plugin update --all && asdf update
+    command -v asdf &>/dev/null && asdf plugin update --all && asdf update
 
-    command -v brew&>/dev/null && brew upgrade
+    command -v brew &>/dev/null && brew upgrade
 
-    command -v gem&>/dev/null && gem update && gem cleanup
+    command -v gem &>/dev/null && gem update && gem cleanup
 
-    command -v cargo&>/dev/null && cargo install cargo-update # To use `install-update` command
-            && cargo install-update -a
-    command -v rustup&>/dev/null && rustup update
+    command -v cargo &>/dev/null && cargo install cargo-update && cargo install-update -a
+    command -v rustup &>/dev/null && rustup update
 
-    command -v pip-review&>/dev/null && pip-review -a
-    command -v pipx&>/dev/null && pipx upgrade-all
+    command -v pip-review &>/dev/null && pip-review -a
+    command -v pipx &>/dev/null && pipx upgrade-all
+end
