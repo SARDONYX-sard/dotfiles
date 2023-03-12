4377163ffaa27372ecb6c64c0964ccca466bd689
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2023-03-13 08:16:48 +0900

    refactor(nvim-git): modify keymap

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -32,19 +32,31 @@ M.plugins = {
   },
 }
 
+---Normal mode keymaps creator function.
+---@param keys string
+---@param func function|string
+---@param desc string
+local nmap = function(keys, func, desc)
+  if desc then
+    desc = 'Git: ' .. desc
+  end
+  vim.keymap.set('n', keys, func, { noremap = true, silent = true, desc = desc })
+end
+
 --commit keymaps
-vim.keymap.set('n', '<leader>gc', ':<C-u>Git commit<CR>', { desc = 'Git: [c]ommit' })
-vim.keymap.set('n', '<leader>gC', ':<C-u>Git commit --amend<CR>', { desc = 'Git: modify prev [c]ommit msg' })
-vim.keymap.set('n', '<leader>gg', ':<C-u>Git commit -S<CR>', { desc = 'Git: commit(with [g]pg)' })
-vim.keymap.set(
-  'n',
-  '<leader>gG',
-  ':<C-u>Git commit --amend -S<CR>',
-  { desc = 'Git: modify prev commit msg(with [G]pg)' }
-)
+nmap('<leader>gc', ':<C-u>Git commit<CR>', '[c]ommit')
+nmap('<leader>gC', ':<C-u>Git commit --amend<CR>', 'modify prev [C]ommit msg')
+
+-- In WSL, if GPG_TTY is not set as in `export GPG_TTY=$(tty)`, the prompt will not appear and the prominent commit will fail.
+-- Therefore, currently the only way to do a prominent commit is to do it manually with a command.
+if not (os.getenv 'WSL_INTEROP' and os.getenv 'WSL_DISTRO_NAME') then
+  nmap('<leader>gg', ':<C-u>Git commit -S<CR>', 'commit(with [g]pg)')
+  nmap('<leader>gG', ':<C-u>Git commit --amend -S<CR>', 'modify prev commit msg(with [G]pg)')
+end
+
 -- other
-vim.keymap.set('n', '<leader>gb', ':<C-u>Git blame<CR>', { desc = 'Git: [b]lame open' })
-vim.keymap.set('n', '<leader>gp', ':<C-u>Git push<CR>', { desc = 'Git: [p]ush to remote' })
-vim.keymap.set('n', '<leader>gs', ':<C-u>Git<CR>', { desc = 'Git: [s]tatus tool open' })
+nmap('<leader>gb', ':<C-u>Git blame<CR>', '[b]lame open')
+nmap('<leader>gp', ':<C-u>Git push<CR>', '[p]ush to remote')
+nmap('<leader>gs', ':<C-u>Git<CR>', '[s]tatus tool open')
 
 return M
