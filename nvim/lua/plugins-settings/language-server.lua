329fe33964c52547da80a6f113c2a954f00a653d
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2023-03-13 06:16:18 +0900

    fix(nvim-hover): fix multi lsp hover warn

--- nvim/lua/plugins-settings/language-server.lua
+++ nvim/lua/plugins-settings/language-server.lua
@@ -196,6 +196,27 @@ cmp.setup {
   },
 }
 
+---Prevent multi hover notifications warning.
+---See: https://github.com/neovim/neovim/issues/20457#issuecomment-1266782345
+---@param _ any
+---@param result table
+---@param ctx table
+---@param config table
+---@return integer|nil|unknown
+vim.lsp.handlers['textDocument/hover'] = function(_, result, ctx, config)
+  config = config or {}
+  config.focus_id = ctx.method
+  if not (result and result.contents) then
+    return
+  end
+  local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
+  markdown_lines = vim.lsp.util.trim_empty_lines(markdown_lines)
+  if vim.tbl_isempty(markdown_lines) then
+    return
+  end
+  return vim.lsp.util.open_floating_preview(markdown_lines, 'markdown', config)
+end
+
 -- Nonattach lsp keymaps
 --
 ---Normal mode lsp keymap creator function.
