c6977a5ee4465bae3a3e8587a47b88dac379b2f8
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2023-03-15 05:52:37 +0900

    refactor(nvim-format): remove unused options

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -1,13 +1,14 @@
--- autoformat.lua
---
 -- Use your language server to automatically format your code on save.
 -- Adds additional commands as well to manage the behavior
 
 local M = {}
 M.plugins = {
-  { -- If an `.editorconfig` file exists in the parent directory, its character encoding, line feed code, etc. must be followed.
+  {
+    -- If an `.editorconfig` file exists in the parent directory, its character encoding,
+    -- line feed code, etc. must be followed.
+    --
     --? Not required since neovim 0.9.0 or higher comes standard.
-    -- see :lua print(vim.fn.has("nvim-0.9.0"))
+    -- See :lua print(vim.fn.has("nvim-0.9.0"))
     'gpanders/editorconfig.nvim',
   },
 
@@ -17,22 +18,8 @@ M.plugins = {
     'https://git.sr.ht/~nedia/auto-format.nvim',
     event = 'BufWinEnter',
     config = function()
-      require('auto-format').setup {
-        -- The name of the augroup.
-        augroup_name = 'AutoFormat',
-
-        -- If formatting takes longer than this amount of time, it will fail. Having no
-        -- timeout at all tends to be ugly - larger files, complex or poor formatters
-        -- will struggle to format within whatever the default timeout
-        -- `vim.lsp.buf.format` uses.
-        timeout = 2000,
-
-        -- These filetypes will not be formatted automatically.
-        exclude_ft = {},
-
-        -- Prefer formatting via LSP for these filetypes.
-        prefer_lsp = {},
-      }
+      -- See: https://git.sr.ht/~nedia/auto-format.nvim/tree/main/item/lua/auto-format/config.lua
+      require('auto-format').setup {}
     end,
   },
 }
