1a7b979d75eda56466fd6b47cb1b0b37900b6276
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2023-03-15 06:24:11 +0900

    feat(nvim-trouble): add keymaps

--- nvim/lua/plugins/language-server.lua
+++ nvim/lua/plugins/language-server.lua
@@ -13,14 +13,50 @@ M.plugins = {
       'williamboman/mason-lspconfig.nvim',
       'williamboman/mason.nvim',
 
-      -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       {
+        -- Standalone UI for nvim-lsp progress.
         'j-hui/fidget.nvim',
         opts = {
           text = {
-            -- animation shown when tasks are ongoing
-            spinner = 'dots2', ---@type 'dots_negative'|'dots_snake'|''
+            ---animation shown when tasks are ongoing
+            --- - See: https://github.com/j-hui/fidget.nvim/blob/main/lua/fidget/spinners.lua
+            --- This alias created with https://github.com/j-hui/fidget.nvim/blob/main/doc/fidget.md#spinners
+            ---@alias Spinner
+            ---|'dots_negative'
+            ---|'dots_snake'
+            ---|'dots_footsteps'
+            ---|'dots_hop'
+            ---|'line'
+            ---|'pipe'
+            ---|'dots_ellipsis'
+            ---|'dots_scrolling'
+            ---|'star'
+            ---|'flip'
+            ---|'hamburger'
+            ---|'grow_vertical'
+            ---|'grow_horizontal'
+            ---|'noise'
+            ---|'dots_bounce'
+            ---|'triangle'
+            ---|'arc'
+            ---|'circle'
+            ---|'square_corners'
+            ---|'circle_quarters'
+            ---|'circle_halves'
+            ---|'dots_toggle'
+            ---|'box_toggle'
+            ---|'arrow'
+            ---|'zip'
+            ---|'bouncing_bar'
+            ---|'bouncing_ball'
+            ---|'clock'
+            ---|'earth'
+            ---|'moon'
+            ---|'dots_pulse'
+            ---|'meter'
+            ---@type Spinner
+            spinner = 'dots_ellipsis',
           },
         },
       },
@@ -42,14 +78,16 @@ M.plugins = {
         ---
         ----See: https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins
         sources = {
-          null_ls.builtins.code_actions.eslint_d, -- js, ts linter
+          null_ls.builtins.code_actions.cspell,
+          null_ls.builtins.code_actions.eslint, -- js, ts linter
+          null_ls.builtins.code_actions.eslint_d, -- js, ts linter faster than eslint
           null_ls.builtins.code_actions.gitsigns,
           null_ls.builtins.code_actions.shellcheck,
           null_ls.builtins.completion.luasnip,
           null_ls.builtins.completion.spell,
-          null_ls.builtins.diagnostics.codespell,
-          null_ls.builtins.diagnostics.gitlint,
+          -- null_ls.builtins.completion.tags,
           null_ls.builtins.diagnostics.eslint_d, -- js, ts linter
+          null_ls.builtins.diagnostics.gitlint,
           null_ls.builtins.diagnostics.mypy, -- static type checker for Python
           null_ls.builtins.diagnostics.ruff, -- fast python linter
           null_ls.builtins.diagnostics.selene, -- fast lua linter
@@ -60,6 +98,16 @@ M.plugins = {
           null_ls.builtins.formatting.prettier, -- js,ts,md,yml,css etc. formatter
           null_ls.builtins.formatting.stylua, -- fast lua formatter
           null_ls.builtins.hover.printenv,
+
+          -- Override configurations
+          null_ls.builtins.diagnostics.cspell.with {
+            diagnostics_postprocess = function(diagnostic)
+              diagnostic.severity = vim.diagnostic.severity['WARN']
+            end,
+            condition = function()
+              return vim.fn.executable 'cspell' > 0
+            end,
+          },
         },
       }
     end,
@@ -70,7 +118,32 @@ M.plugins = {
     requires = 'nvim-tree/nvim-web-devicons',
     config = function()
       require('trouble').setup {}
-      vim.keymap.set('n', '<leader>d', ':TroubleToggle<CR>', { desc = 'Show errors([D]iabonostics)' })
+
+      local ok, wk = pcall(require, 'which-key')
+      if ok then
+        wk.register {
+          ['<leader>x'] = {
+            name = '+Diagnostics',
+          },
+        }
+      end
+
+      ---Normal mode diagnostics keymap creator function.
+      ---@param keys string
+      ---@param func function|string
+      ---@param desc string
+      local nmap = function(keys, func, desc)
+        if desc then
+          desc = 'Diagnostics: ' .. desc
+        end
+        vim.keymap.set('n', keys, func, { silent = true, desc = desc })
+      end
+      -- NOTE: Override builtin diagnostics if trouble.nvim is enabled.
+      nmap('<leader>d', ':TroubleToggle<CR>', '[d]iagnostics')
+      nmap('<leader>xw', ':TroubleToggle workspace_diagnostics<CR>', '[w]orkspace')
+      nmap('<leader>xD', ':TroubleToggle document_diagnostics<CR>', '[D]ocuments')
+      nmap('<leader>xl', ':TroubleToggle loclist<CR>', '[l]ocations')
+      nmap('<leader>xq', ':TroubleToggle quickfix<CR>', '[q]uikfixes')
     end,
   },
 
