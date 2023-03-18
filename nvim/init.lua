df49967c6ff83e9c1d52cdb20cd2eff59305a422
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2023-03-19 08:30:12 +0900

    feat(nvim): add `open dir with file manager` function

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,40 @@
--- Edit vimrc keymap
+-- NOTE: Always load these keymaps first to make it easier to correct and edit plugin settings in case of errors.
 vim.keymap.set('n', '<space>v', function()
   -- I'm clenching errors in pcall to prevent buff errors in `editorconfig.nvim`.
   pcall(vim.cmd.edit, debug.getinfo(1, 'S').short_src)
 end, { desc = '[v]iew & edit init.lua' })
+
+vim.keymap.set('n', '<space>V', function()
+  local pwd = vim.fn.expand '%:p:h'
+
+  if vim.fn.executable 'explorer.exe' == 1 and vim.fn.executable 'powershell.exe' == 1 then
+    if vim.fn.has 'win32' == 1 then
+      vim.fn.system('explorer.exe ' .. pwd)
+      return
+    else
+      if vim.fn.has 'wsl' == 1 then
+        pwd = vim.fn.system("bash -c 'wslpath -w " .. pwd .. "'")
+      elseif vim.fn.has 'win32unix' == 1 then
+        pwd = vim.fn.system("bash -c 'cygwinpath -w " .. pwd .. "'")
+      end
+      -- NOTE: Cannot jump to path location correctly without powershell.
+      vim.fn.system("powershell.exe -c 'explorer.exe " .. pwd .. "'")
+      return
+    end
+  elseif vim.fn.has 'linux' == 1 and vim.fn.executable 'xdg-open' == 1 then
+    vim.fn.system('xdg-open ' .. pwd)
+    return
+  elseif vim.fn.has 'mac' == 1 and vim.fn.executable 'open' == 1 then
+    vim.fn.system('open ' .. pwd)
+    return
+  end
+
+  vim.notify(
+    'Failed to open the directory(' .. pwd .. ') because there is no expected command',
+    vim.log.levels.ERROR,
+    { title = 'init.lua' }
+  )
+end, { desc = '[V]iew(Open) current dir with GUI' })
 
 -- Imports dependencies
 local utils = {
