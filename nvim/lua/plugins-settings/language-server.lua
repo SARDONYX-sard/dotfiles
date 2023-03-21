6a415157b25098a408afc79ea480b7b44d949650
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2023-03-21 12:37:10 +0900

    fix(nvim-hover): fix hover error

--- nvim/lua/plugins-settings/language-server.lua
+++ nvim/lua/plugins-settings/language-server.lua
@@ -208,7 +208,10 @@ vim.lsp.handlers['textDocument/hover'] = function(_, result, ctx, config)
   if not (result and result.contents) then
     return
   end
-  local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines('MarkupContent', result.contents)
+  -- NOTE: Ignore the warning because the display will error if you match the type with 'MarkdownContent' or other types.
+  --
+  ---@diagnostic disable-next-line: missing-parameter
+  local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
   markdown_lines = vim.lsp.util.trim_empty_lines(markdown_lines)
   if vim.tbl_isempty(markdown_lines) then
     return
