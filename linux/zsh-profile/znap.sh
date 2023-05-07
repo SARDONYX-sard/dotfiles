509fedd0bb1b0169e8028d20334ddfbc3f79e26a
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2023-05-07 14:23:42 +0900

    fix(linux-znap): fix znap error

--- linux/zsh-profile/znap.sh
+++ linux/zsh-profile/znap.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 
-zsh_plugins_dir="$HOME/.cache/zsh/plugins"
+zsh_plugins_dir="${HOME}/.cache/zsh/plugins"
 
 # Download Znap, if it's not there yet.
-[[ -f "$zsh_plugins_dir"/zsh-snap/znap.zsh ]] ||
+[[ -f "${zsh_plugins_dir}"/zsh-snap/znap.zsh ]] ||
   git clone --depth 1 -- \
     https://github.com/marlonrichert/zsh-snap.git \
-    "$zsh_plugins_dir"/zsh-snap
+    "${zsh_plugins_dir}"/zsh-snap
 
-source "$zsh_plugins_dir"/zsh-snap/znap.zsh # Start Znap
+source "${zsh_plugins_dir}"/zsh-snap/znap.zsh # Start Znap
 
 function read_znap() {
   local plugin="$1"
+  local plugin_name
   plugin_name=$(echo "${plugin}" | awk -F "/" '{ print $NF }')
-  [ ! -e "$zsh_plugins_dir"/"$plugin_name" ] && znap install "$plugin"
+  local plugin_dir="${zsh_plugins_dir}"/"${plugin_name}"
+
+  [[ ! -e ${plugin_dir} ]] && znap install "${plugin}"
 
   #! msys2 will give an error, so load directly.
-  source "$zsh_plugins_dir"/"$plugin_name"/"$plugin_name".plugin.zsh
+  source "${plugin_dir}/${plugin_name}".plugin.zsh
 }
 
 plugins=(
@@ -27,16 +30,12 @@ plugins=(
 
 # `znap source` automatically downloads and starts your plugins.
 for plugin in "${plugins[@]}"; do
-  if [ -e /mnt/c ]; then
-    znap source "${plugin}" # `znap source` automatically downloads and starts your plugins.
-  else
-    read_znap "${plugin}"
-  fi
+  read_znap "${plugin}"
 done
 
 #! Manual loading because the plugin manager does not read it well.
 znap install lincheney/fzf-tab-completion
-source "$zsh_plugins_dir"/fzf-tab-completion/zsh/fzf-zsh-completion.sh
+source "${zsh_plugins_dir}"/fzf-tab-completion/zsh/fzf-zsh-completion.sh
 
 # `znap eval` caches and runs any kind of command output for you.
 znap eval iterm2 'curl -fsSL https://iterm2.com/shell_integration/zsh'
