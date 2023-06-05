62580e580cf9819ce6c085a2cf9e8260249c3ad9
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2023-06-05 23:25:39 +0900

    fix(win): fix parse encode

--- windows/config/powershell-profile/helper/index.lazy.ps1
+++ windows/config/powershell-profile/helper/index.lazy.ps1
@@ -1,30 +1,3 @@
-# creates a new function dynamically
-function New-DynamicFunction {
-  [OutputType([System.Management.Automation.CompletionResult])]  # zero to many
-  param(
-    [string] $CommandName,
-    [string] $FunctionBody,
-    [scriptblock] $ScriptBlock
-  )
-
-  if ($FunctionBody -eq $null) {
-    $Body = $ScriptBlock.ToString()
-  }
-  elseif ($ScriptBlock -eq $null) {
-    $Body = $FunctionBody
-  }
-  else {
-    $Body = ""
-  }
-  # create new function in function: drive and set scope to "script:"
-  $null = New-Item -Path function: -Name "script:$CommandName" -Value $Body
-}
-
-if ($PSVersionTable.PSEdition -eq "Core") {
-  # https://docs.microsoft.com/powershell/module/psreadline/about/about_psreadline?view=powershell-7.2
-  Set-PSReadLineOption -PredictionSource History #* Core only module
-}
-
 @("common", "linux-compatible", "powershell-only") | ForEach-Object {
   $local:ImportFullDirectory = "$HelperDir\$_";
 
@@ -34,7 +7,7 @@ if ($PSVersionTable.PSEdition -eq "Core") {
 
     Get-ChildItem -Path $ImportFullDirectory -File -Recurse -Include "*.dyn.lazy.ps1" |
     ForEach-Object {
-      $FnBody = Get-Content $_.FullName -Raw
+      $FnBody = Get-Content $_.FullName -Raw -Encoding utf8
       # Write-Host $_.Name.Replace(".dyn.lazy.ps1", "")
       New-DynamicFunction -CommandName $_.Name.Replace(".dyn.lazy.ps1", "") -FunctionBody $FnBody
     }
--- windows/config/powershell-profile/helper/index.ps1
+++ windows/config/powershell-profile/helper/index.ps1
@@ -1,3 +1,25 @@
+# creates a new function dynamically
+function New-DynamicFunction {
+  [OutputType([System.Management.Automation.CompletionResult])]  # zero to many
+  param(
+    [string] $CommandName,
+    [string] $FunctionBody,
+    [scriptblock] $ScriptBlock
+  )
+
+  if ($FunctionBody -eq $null) {
+    $Body = $ScriptBlock.ToString()
+  }
+  elseif ($ScriptBlock -eq $null) {
+    $Body = $FunctionBody
+  }
+  else {
+    $Body = ""
+  }
+  # create new function in function: drive and set scope to "script:"
+  $null = New-Item -Path function: -Name "script:$CommandName" -Value $Body
+}
+
 @("common", "linux-compatible", "powershell-only") | ForEach-Object {
   $local:ImportFullDirectory = "$HelperDir\$_";
   if (Test-Path $ImportFullDirectory) {
