72409b5ac871eb998ebebe383adbb50f4f6589b7
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2023-06-05 21:25:06 +0900

    refactor(win): gather code

--- windows/config/powershell-profile/helper/enhanced-lazy.ps1
+++ windows/config/powershell-profile/helper/enhanced-lazy.ps1
@@ -17,22 +17,20 @@ $LazyLoadProfileRunspace.Open()
 [void]$LazyLoadProfile.BeginInvoke()
 
 $null = Register-ObjectEvent -InputObject $LazyLoadProfile -EventName InvocationStateChanged -Action {
-  if ($PSVersionTable.PSEdition -eq "Core") {
+  if ($PSVersionTable.PSEdition -eq 'Core') {
     oh-my-posh --init --shell pwsh --config "$HOME\dotfiles\common\data\oh-my-posh-themes\my-custom.json" | Invoke-Expression
     Import-Module posh-git
+    # https://docs.microsoft.com/powershell/module/psreadline/about/about_psreadline?view=powershell-7.2
+    Set-PSReadLineOption -PredictionSource History #* Core only module
   }
-  @("behaviors", "external-modules-settings") | ForEach-Object {
+  @('behaviors', 'external-modules-settings') | ForEach-Object {
     $local:ImportFullDirectory = "$HelperDir\$_";
     if (Test-Path $ImportFullDirectory) {
-      Get-ChildItem -Path $ImportFullDirectory -File -Recurse -Include "*.ps1" |
+      Get-ChildItem -Path $ImportFullDirectory -File -Recurse -Include '*.ps1' |
       ForEach-Object {
         . $_.FullName
       }
     }
-  }
-  if ($PSVersionTable.PSEdition -eq "Core") {
-    # https://docs.microsoft.com/powershell/module/psreadline/about/about_psreadline?view=powershell-7.2
-    Set-PSReadLineOption -PredictionSource History #* Core only module
   }
 
   $LazyLoadProfile.Dispose()
