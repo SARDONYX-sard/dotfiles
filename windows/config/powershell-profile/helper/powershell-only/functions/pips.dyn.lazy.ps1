afe665b00453b8633fb208efdc126c766b1b358d
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2023-06-05 21:38:12 +0900

    refactor(win-hooks): move pip to hooks

--- windows/config/powershell-profile/helper/powershell-only/functions/pips.dyn.lazy.ps1
+++
@@ -1,3 +0,0 @@
-if ((Get-Command pip -ErrorAction SilentlyContinue) -and (Get-Command pip_search -ErrorAction SilentlyContinue)) {
-  pip_search $args
-}
---
+++ windows/config/powershell-profile/helper/powershell-only/hooks/pip.dyn.lazy.ps1
@@ -0,0 +1,8 @@
+if (Get-Command pip_search -ErrorAction SilentlyContinue) {
+  if ($args[0] -eq "search") {
+    pip_search @($args | Select-Object -Skip 1)
+  }
+  else {
+    pip.exe @args
+  }
+}
