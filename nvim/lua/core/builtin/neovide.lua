37a3490293400aa577e8d947534d428da920ba92
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2023-07-02 04:54:13 +0900

    feat(nvim-neovide): add type annotation

--- nvim/lua/core/builtin/neovide.lua
+++ nvim/lua/core/builtin/neovide.lua
@@ -1,16 +1,17 @@
 -- https://neovide.dev/configuration.html
 if vim.fn.exists(vim.g.neovide) == 1 then
   local M = {}
-  M.font_settings = 'CodeNewRoman NF:h13'
+  M.default_font = 'CodeNewRoman NF:h13'
 
   vim.g.neovide_cursor_vfx_mode = 'railgun'
   vim.g.neovide_cursor_vfx_mode = 'wireframe'
   vim.g.neovide_refresh_rate = 60
   vim.g.neovide_transparency = 0.6
-  vim.o.guifont = M.font_settings
+  vim.o.guifont = M.default_font
 
   ---@param size number
   function M.change_font_size(size)
+    ---@type string|nil, string|nil
     local font_name, font_size = vim.o.guifont:match '^(.+):h(%d+)$'
 
     if font_name and font_size then
@@ -29,7 +30,7 @@ if vim.fn.exists(vim.g.neovide) == 1 then
   end
 
   function M.reset_font()
-    vim.o.guifont = M.font_settings
+    vim.o.guifont = M.default_font
   end
 
   vim.keymap.set('n', '<leader>0', M.reset_font, { desc = 'Font: reset', noremap = true })
