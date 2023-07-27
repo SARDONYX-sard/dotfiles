4452f13a3e106965a18bc3364ed0048d3890a71c
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2023-07-27 09:49:14 +0900

    fix(linux-installer): fix neovim installer error

--- linux/bin/installers/neovim.sh
+++ linux/bin/installers/neovim.sh
@@ -1,26 +1,5 @@
 #!/usr/bin/env bash
 
-help=@"
--f | --force: Force install neovim.
--h | --help : Show this help.
-"@
-
-while (($# > 0)); do
-  case "${1}" in
-  -f | --force)
-    force="true"
-    shift # shift once since flags have no values
-    ;;
-  -h | --help)
-    echo "${help}"
-    exit 0
-    ;;
-  *) # unknown flag/switch
-    shift
-    ;;
-  esac
-done
-
 function check_deps() {
   declare -a deps=("command" "tar" "curl" "arch")
   local no_deps="false"
@@ -34,25 +13,87 @@ function check_deps() {
 
   [[ "${no_deps}" == "true" ]] && exit 1
 }
+
+function remove_if_exists() {
+  for target in "${@}"; do
+    [[ -e "${target}" ]] && sudo /bin/rm -rf "${target}"
+  done
+}
+
+function info() {
+  printf "\033[1;34m[Info]\033[0m: %s\n" "${@}"
+}
+function ok() {
+  printf "\033[1;32m[Success]\033[0m: %s\n" "${@}"
+}
+function warn() {
+  printf "\033[1;33m[Warn]\033[0m: %s\n" "${@}"
+}
+function err() {
+  printf "\033[1;31m[Error]\033[0m: %s\n" "${@}"
+}
+
+while (($# > 0)); do
+  case "${1}" in
+  -f | --force)
+    force="true"
+    shift # shift once since flags have no values
+    ;;
+  -h | --help)
+    help="
+-f | --force: Force install neovim.
+-h | --help : Show this help.
+"
+    info "Manual latest Neovim installer from tar.gz for x86_64."
+    echo "${help}"
+    exit 0
+    ;;
+  *) # unknown flag/switch
+    shift
+    ;;
+  esac
+done
+
 check_deps
 
 arch=$(arch)
-[[ "${arch}" != "x86_64" ]] && printf "This installer only x86_64.\n" && exit 1
+[[ "${arch}" != "x86_64" ]] && err "This installer only x86_64." && exit 1
+([[ "${force}" != "true" ]] && command -v nvim) >/dev/null 2>&1 &&
+  warn "neovim already installed." && exit 0
 
-([[ "${force}" != "true" ]] && command -v nvim) >/dev/null 2>&1 && printf "neovim already installed.\n" && exit 0
-
+nvim_share="/usr/share/share/nvim/"
 if [[ "${force}" == "true" ]]; then
-  if (command -v nvim) >/dev/null 2>&1; then
-    printf "UnInstalling neovim...\n"
-    sudo /bin/rm -rf "usr/share/nvim/" "${HOME}/nvim-linux64"
-  fi
+  info "UnInstalling previous neovim dirs if exists..."
+  remove_if_exists "${nvim_share}" "/usr/local/bin/nvim"
 fi
 
-printf "Installing neovim...\n"
-curl -OL https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz --output-dir "${HOME}" -o nvim-linux64.tar.gz
-tar zxf "${HOME}"/nvim-linux64.tar.gz -C "${HOME}"
-sudo mv -f "${HOME}/nvim-linux64/bin/nvim" /usr/local/bin/
-sudo -p /usr/share # -p: preserve group verctor
-sudo mv -f "${HOME}/nvim-linux64/share/" "/usr/share/"
-/bin/rm "${HOME}/nvim-linux64.tar.gz" "${HOME}/nvim-linux64"
-printf "Installed neovim.\n"
+extract_dir="${HOME}/nvim-linux64/"
+tar_filename="nvim-linux64.tar.gz"
+tar_path="${HOME}/${tar_filename}"
+
+if [[ ! -e "${extract_dir}" ]]; then
+  info "Not found cache. Download start."
+  curl -OL https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz \
+    --output-dir "${HOME}" -o "${tar_filename}"
+  info "Extracting ${tar_filename}..."
+  tar zxf "${tar_path}" -C "${HOME}"
+fi
+
+info "Installing neovim..."
+sudo mv -f "${extract_dir}/bin/nvim" /usr/local/bin/
+sudo mv -f "${extract_dir}/share/" "/usr/share/share/nvim"
+
+if [[ -e "${nvim_share}" ]]; then
+  info "move ${nvim_share} completed."
+  info "Removing tmp & downloaded files..."
+  remove_if_exists "${extract_dir}" "${tar_path}"
+else
+  red "Failed to move ${nvim_share}"
+  remove_if_exists "${tar_path}"
+  info "Remain ${extract_dir} to retry install."
+fi
+
+echo "--------------------------------------------------"
+nvim --version
+echo "--------------------------------------------------"
+ok "Installed neovim."
