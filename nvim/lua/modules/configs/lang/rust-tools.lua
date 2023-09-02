de43087aae4fe76e7f8b65fc05db4018972a7b1f
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2023-09-03 00:04:48 +0900

    feat(nvim-rust-tools): add conditinal branch
    For inlay hint(nightly feature)

--- nvim/lua/modules/configs/lang/rust-tools.lua
+++ nvim/lua/modules/configs/lang/rust-tools.lua
@@ -29,14 +29,14 @@ return function()
       inlay_hints = {
         -- automatically set inlay hints (type hints)
         -- default: true
-        auto = true,
+        auto = vim.lsp.inlay_hint == nil,
 
         -- Only show inlay hints for the current line
         only_current_line = false,
 
         -- whether to show parameter hints with the inlay hints or not
         -- default: true
-        show_parameter_hints = true,
+        show_parameter_hints = vim.lsp.inlay_hint == nil,
 
         -- prefix for parameter hints
         -- default: "<-"
