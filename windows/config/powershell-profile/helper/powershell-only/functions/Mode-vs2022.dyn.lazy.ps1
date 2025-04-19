994b32894166eed97771d140511891b396bacffe
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2025-04-19 23:06:30 +0900

    fix: update PowerShell script to use vswhere for Visual Studio path resolution

--- windows/config/powershell-profile/helper/powershell-only/functions/Mode-vs2022.dyn.lazy.ps1
+++ windows/config/powershell-profile/helper/powershell-only/functions/Mode-vs2022.dyn.lazy.ps1
@@ -1,3 +1,5 @@
 # Enable clang, vcpkg path
 # See: https://learn.microsoft.com/visualstudio/ide/reference/command-prompt-powershell?view=vs-2022
-& { Import-Module $env:VS2022INSTALLDIR/Common7/Tools/Microsoft.VisualStudio.DevShell.dll; Enter-VsDevShell 3f90b5ca -SkipAutomaticLocation -Arch amd64 -HostArch amd64 }
+#
+# https://stackoverflow.com/questions/68635853/how-do-i-get-visual-studio-developer-powershell-working-in-visual-studio-codes
+$vsPath = &(Join-Path 'C:\\Program Files (x86)' '\\Microsoft Visual Studio\\Installer\\vswhere.exe') -property installationpath; Import-Module (Join-Path $vsPath 'Common7\\Tools\\Microsoft.VisualStudio.DevShell.dll'); Enter-VsDevShell -VsInstallPath $vsPath -SkipAutomaticLocation -Arch amd64 -HostArch amd64
